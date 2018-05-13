50b4a373e29f24d5f1e52f8836756cf24fd34fbd
Michael Koenig <koenigm@seattleu.edu>
2018-05-13 14:13:02 -0700

    Added Data Annotations for Student

--- 5051/5051/Models/StudentModel.cs
+++ 5051/5051/Models/StudentModel.cs
@@ -2,13 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace _5051.Models
 {
     public class StudentModel
     {
-        public string Id;
-        public string Name;
-        public string AvatarId;
+
+        [Display(Name = "Id", Description = "Student Id")]
+        [Required(ErrorMessage = "Id is required")]
+        public string Id { get; set; }
+
+        [Display(Name = "Name", Description = "Nick Name")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "AvatarId", Description = "Avatar")]
+        [Required(ErrorMessage = "Avatar is required")]
+        public string AvatarId { get; set; }
+
+        [Display(Name = "Tokens", Description = "Tokens Saved")]
+        [Required(ErrorMessage = "Tokens are required")]
+        public int Tokens { get; set; }
+
     }
 }
