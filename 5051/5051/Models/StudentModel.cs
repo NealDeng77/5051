4a0c9fc1f152efe89d20d872a41820103d7d721f
Michael Koenig <koenigm@seattleu.edu>
2018-05-13 14:30:04 -0700

    Updated Student Model to have AvatarID call the avatarbackend to get the default it needed.

--- 5051/5051/Models/StudentModel.cs
+++ 5051/5051/Models/StudentModel.cs
@@ -6,6 +6,8 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 
+using _5051.Backend;
+
 namespace _5051.Models
 {
     public class StudentModel
@@ -23,6 +25,10 @@ namespace _5051.Models
         [Required(ErrorMessage = "Avatar is required")]
         public string AvatarId { get; set; }
 
+        [Display(Name = "Avatar Level", Description = "Level of the Avatar")]
+        [Required(ErrorMessage = "Level is required")]
+        public int AvatarLevel { get; set; }
+
         [Display(Name = "Tokens", Description = "Tokens Saved")]
         [Required(ErrorMessage = "Tokens are required")]
         public int Tokens { get; set; }
@@ -32,15 +38,22 @@ namespace _5051.Models
         {
             Id = Guid.NewGuid().ToString();
             Tokens = 0;
+            AvatarLevel = 0;
         }
 
+        /// <summary>
+        /// Constructor for Student.  Call this when making a new student
+        /// </summary>
+        /// <param name="name">The Name to call the student</param>
+        /// <param name="avatarId">The avatar to use, if not specified, will call the backend to get an ID</param>
         public StudentModel(string name, string avatarId)
         {
             Name = name;
 
+            // If no avatar ID is sent in, then go and get the first avatar ID from the backend data as the default to use.
             if (string.IsNullOrEmpty(avatarId))
             {
-                avatarId = ""
+                avatarId = AvatarBackend.Instance.GetFirstAvatarId();
             }
             AvatarId = avatarId;
         }
