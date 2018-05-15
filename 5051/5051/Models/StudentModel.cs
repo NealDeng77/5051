a6c6791ebf9c9681fd50725190cb591fcceefa3f
Michael Koenig <koenigm@seattleu.edu>
2018-05-14 18:56:47 -0700

    Comments for StudentModel

--- 5051/5051/Models/StudentModel.cs
+++ 5051/5051/Models/StudentModel.cs
@@ -15,27 +15,45 @@ namespace _5051.Models
     /// </summary>
     public class StudentModel
     {
+        /// <summary>
+        /// The ID for the Student, this is the key, and a required field
+        /// </summary>
         [Key]
         [Display(Name = "Id", Description = "Student Id")]
         [Required(ErrorMessage = "Id is required")]
         public string Id { get; set; }
 
+        /// <summary>
+        /// The Friendly name for the student, does not need to be directly associated with the actual student name
+        /// </summary>
         [Display(Name = "Name", Description = "Nick Name")]
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// The ID of the Avatar the student is associated with, this will convert to an avatar picture
+        /// </summary>
         [Display(Name = "AvatarId", Description = "Avatar")]
         [Required(ErrorMessage = "Avatar is required")]
         public string AvatarId { get; set; }
 
+        /// <summary>
+        /// The personal level for the Avatar, the avatar levels up.  switching the avatar ID (picture), does not change the level
+        /// </summary>
         [Display(Name = "Avatar Level", Description = "Level of the Avatar")]
         [Required(ErrorMessage = "Level is required")]
         public int AvatarLevel { get; set; }
 
+        /// <summary>
+        /// The number of Tokens the student has, tokens are used in the store, and also to level up
+        /// </summary>
         [Display(Name = "Tokens", Description = "Tokens Saved")]
         [Required(ErrorMessage = "Tokens are required")]
         public int Tokens { get; set; }
 
+        /// <summary>
+        /// The status of the student, for example currently logged in, out
+        /// </summary>
         [Display(Name = "Current Status", Description = "Status of the Student")]
         [Required(ErrorMessage = "Status is required")]
         public StudentStatusEnum Status { get; set; }
@@ -120,15 +138,27 @@ namespace _5051.Models
     /// </summary>
     public class StudentDisplayViewModel : StudentModel
     {
+        /// <summary>
+        /// The path to the local image for the avatar
+        /// </summary>
         [Display(Name = "Avatar Picture", Description = "Avatar Picture to Show")]
         public string AvatarUri { get; set; }
 
+        /// <summary>
+        /// Display name for the Avatar on the student information (Friendly Police etc.)
+        /// </summary>
         [Display(Name = "Avatar Name", Description = "Avatar Name")]
         public string AvatarName { get; set; }
 
+        /// <summary>
+        /// Description of the Avatar to show on the student information
+        /// </summary>
         [Display(Name = "Avatar Description", Description = "Avatar Description")]
         public string AvatarDescription { get; set; }
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public StudentDisplayViewModel() { }
 
         /// <summary>
