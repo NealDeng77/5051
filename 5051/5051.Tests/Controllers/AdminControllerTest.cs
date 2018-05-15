12bd8b05eb97091e8f927a8abd5c143c3504cfab
Michael Koenig <koenigm@seattleu.edu>
2018-05-14 19:22:08 -0700

    Added Admin Contoller Test

---
+++ 5051/5051.Tests/Controllers/AdminControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051;
+using _5051.Controllers;
+
+namespace _5051.Tests.Controllers
+{
+    [TestClass]
+    public class AdminControllerTest
+    {
+        [TestMethod]
+        public void Controller_Admin_Index_Default_Should_Pass()
+        {
+            // Arrange
+            AdminController controller = new AdminController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
