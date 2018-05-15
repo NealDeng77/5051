9493327d4fdda1452a3bf026bd5a974d9c3ab9f6
Michael Koenig <koenigm@seattleu.edu>
2018-05-14 20:12:31 -0700

    Adding Avatar Controller UT

--- 5051/5051.Tests/Controllers/AvatarControllerTest.cs
+++ 5051/5051.Tests/Controllers/AvatarControllerTest.cs
@@ -30,7 +30,7 @@ namespace _5051.Tests.Controllers
         }
 
         [TestMethod]
-        public void Controller_Avatar_Read_ID_Null_Should_Return_Error()
+        public void Controller_Avatar_Read_ID_Null_Should_Return_Empty_Model()
         {
             // Arrange
             AvatarController controller = new AvatarController();
@@ -45,7 +45,7 @@ namespace _5051.Tests.Controllers
 
 
         [TestMethod]
-        public void Controller_Avatar_Read_ID_Bogus_Should_Return_Error()
+        public void Controller_Avatar_Read_ID_Bogus_Should_Return_Empty_Model()
         {
             // Arrange
             AvatarController controller = new AvatarController();
@@ -59,11 +59,12 @@ namespace _5051.Tests.Controllers
         }
 
         [TestMethod]
-        public void Controller_Avatar_Read_ID_Valid_Should_Return_Error()
+        public void Controller_Avatar_Read_ID_Valid_Should_Pass()
         {
             // Arrange
             AvatarController controller = new AvatarController();
             
+            // Get the first Avatar from the DataSource
             string id = AvatarBackend.Instance.GetFirstAvatarId();
 
             // Act
@@ -75,47 +76,119 @@ namespace _5051.Tests.Controllers
             Assert.AreEqual(id, resultAvatar.Id, TestContext.TestName);
         }
 
+        [TestMethod]
+        public void Controller_Avatar_Create_Get_Should_Return_New_Model()
+        {
+            // Arrange
+            AvatarController controller = new AvatarController();
 
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
 
-    //    public ActionResult Create()
-    //    {
-    //        var myData = new AvatarModel();
-    //        return View(myData);
-    //    }
+            var resultAvatar = result.Model as AvatarModel;
 
-    //    public ActionResult Create([Bind(Include=
-    //                                    "Id,"+
-    //                                    "Name,"+
-    //                                    "Description,"+
-    //                                    "Uri,"+
-    //                                    "")] AvatarModel data)
+            // Assert
+            Assert.AreNotEqual(null, resultAvatar.Id, TestContext.TestName);
+        }
 
-    //    public ActionResult Update(string id = null)
-    //    {
-    //        var myData = avatarBackend.Read(id);
-    //        return View(myData);
-    //    }
+        [TestMethod]
+        public void Controller_Avatar_Create_Post_Invalid_Null_Id_Should_Return_Model()
+        {
+            // Arrange
+            AvatarController controller = new AvatarController();
+            var data = new AvatarModel
+            {
+                Description = "description",
+                Id = null,
+                Name = "Name",
+                Uri = "picture"
+            };
 
-    //    public ActionResult Update([Bind(Include=
-    //                                    "Id,"+
-    //                                    "Name,"+
-    //                                    "Description,"+
-    //                                    "Uri,"+
-    //                                    "")] AvatarModel data)
+            // Act
+            ViewResult result = controller.Create(data) as ViewResult;
 
-    //    public ActionResult Delete(string id = null)
-    //    {
-    //        var myData = avatarBackend.Read(id);
-    //        return View(myData);
-    //    }
+            var resultAvatar = result.Model as AvatarModel;
 
-    //    [HttpPost]
-    //    public ActionResult Delete([Bind(Include=
-    //                                    "Id,"+
-    //                                    "Name,"+
-    //                                    "Description,"+
-    //                                    "Uri,"+
-    //                                    "")] AvatarModel data)
+            // Assert
+            Assert.AreEqual(data.Description, resultAvatar.Description, TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Controller_Avatar_Create_Post_Valid_Should_Return_Index_Page()
+        {
+            // Arrange
+            AvatarController controller = new AvatarController();
+
+            var data = new AvatarModel
+            {
+                Description = "description",
+                Id = "abc",
+                Name = "Name",
+                Uri = "picture"
+            };
+
+            // Act
+            var result = (RedirectToRouteResult) controller.Create(data);
+
+            // Assert
+            Assert.AreEqual("Index", result.RouteValues["action"], TestContext.TestName);
+            // No need to check the route, Assert.AreEqual("Avatar", result.RouteValues["route"], TestContext.TestName);
+
+            // Check that the item is created
+            var resultAvatar = AvatarBackend.Instance.Read("abc");
+            Assert.AreEqual(data.Id, resultAvatar.Id, TestContext.TestName);
+
+            // Reset the Avatars
+            AvatarBackend.Instance.Reset();
+
+        }
+
+        [TestMethod]
+        public void Controller_Avatar_Create_Post_InValid_Should_Return_Error_Page()
+        {
+            /// <summary>
+            /// This Test calls the create, but passes null data
+            /// The controller will return a redirect to the error home page
+            /// So the test needs to cast the return to a redirect, and then check that it got to the home error page
+            /// </summary>
+
+            // Arrange
+            AvatarController controller = new AvatarController();
+
+            // Act
+            var result = (RedirectToRouteResult)controller.Create(null);
+
+            // Assert
+            Assert.AreEqual("Error", result.RouteValues["action"], TestContext.TestName);
+            Assert.AreEqual("Home", result.RouteValues["route"], TestContext.TestName);
+        }
+
+        //    public ActionResult Update(string id = null)
+        //    {
+        //        var myData = avatarBackend.Read(id);
+        //        return View(myData);
+        //    }
+
+        //    public ActionResult Update([Bind(Include=
+        //                                    "Id,"+
+        //                                    "Name,"+
+        //                                    "Description,"+
+        //                                    "Uri,"+
+        //                                    "")] AvatarModel data)
+
+        //    public ActionResult Delete(string id = null)
+        //    {
+        //        var myData = avatarBackend.Read(id);
+        //        return View(myData);
+        //    }
+
+        //    [HttpPost]
+        //    public ActionResult Delete([Bind(Include=
+        //                                    "Id,"+
+        //                                    "Name,"+
+        //                                    "Description,"+
+        //                                    "Uri,"+
+        //                                    "")] AvatarModel data)
 
     }
 }
