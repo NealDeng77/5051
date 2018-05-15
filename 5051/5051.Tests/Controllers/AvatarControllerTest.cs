adddd5f850e1903912a42b8ff239f81f681a2c5f
Michael Koenig <koenigm@seattleu.edu>
2018-05-15 08:36:08 -0700

    Adding Update UT, partial Checkin

--- 5051/5051.Tests/Controllers/AvatarControllerTest.cs
+++ 5051/5051.Tests/Controllers/AvatarControllerTest.cs
@@ -16,6 +16,7 @@ namespace _5051.Tests.Controllers
     {
         public TestContext TestContext { get; set; }
 
+        #region IndexRegion
         [TestMethod]
         public void Controller_Avatar_Index_Default_Should_Pass()
         {
@@ -26,9 +27,11 @@ namespace _5051.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.IsNotNull(result,TestContext.TestName);
+            Assert.IsNotNull(result, TestContext.TestName);
         }
+        #endregion IndexRegion
 
+        #region ReadRegion
         [TestMethod]
         public void Controller_Avatar_Read_ID_Null_Should_Return_Empty_Model()
         {
@@ -42,7 +45,6 @@ namespace _5051.Tests.Controllers
             // Assert
             Assert.AreEqual(null, result.Model, TestContext.TestName);
         }
-
 
         [TestMethod]
         public void Controller_Avatar_Read_ID_Bogus_Should_Return_Empty_Model()
@@ -63,7 +65,7 @@ namespace _5051.Tests.Controllers
         {
             // Arrange
             AvatarController controller = new AvatarController();
-            
+
             // Get the first Avatar from the DataSource
             string id = AvatarBackend.Instance.GetFirstAvatarId();
 
@@ -76,6 +78,9 @@ namespace _5051.Tests.Controllers
             Assert.AreEqual(id, resultAvatar.Id, TestContext.TestName);
         }
 
+        #endregion ReadRegion
+
+        #region CreateRegion
         [TestMethod]
         public void Controller_Avatar_Create_Get_Should_Return_New_Model()
         {
@@ -128,7 +133,7 @@ namespace _5051.Tests.Controllers
             };
 
             // Act
-            var result = (RedirectToRouteResult) controller.Create(data);
+            var result = (RedirectToRouteResult)controller.Create(data);
 
             // Assert
             Assert.AreEqual("Index", result.RouteValues["action"], TestContext.TestName);
@@ -162,6 +167,97 @@ namespace _5051.Tests.Controllers
             Assert.AreEqual("Error", result.RouteValues["action"], TestContext.TestName);
             Assert.AreEqual("Home", result.RouteValues["route"], TestContext.TestName);
         }
+        #endregion CreateRegion
+
+        #region UpdateRegion
+        //[TestMethod]
+        //public void Controller_Avatar_Update_Get_Should_Return_New_Model()
+        //{
+        //    // Arrange
+        //    AvatarController controller = new AvatarController();
+
+        //    // Act
+        //    ViewResult result = controller.Update() as ViewResult;
+
+        //    var resultAvatar = result.Model as AvatarModel;
+
+        //    // Assert
+        //    Assert.AreNotEqual(null, resultAvatar.Id, TestContext.TestName);
+        //}
+
+        [TestMethod]
+        public void Controller_Avatar_Update_Post_Invalid_Null_Id_Should_Return_Model()
+        {
+            // Arrange
+            AvatarController controller = new AvatarController();
+            var data = new AvatarModel
+            {
+                Description = "description",
+                Id = null,
+                Name = "Name",
+                Uri = "picture"
+            };
+
+            // Act
+            ViewResult result = controller.Update(data) as ViewResult;
+
+            var resultAvatar = result.Model as AvatarModel;
+
+            // Assert
+            Assert.AreEqual(data.Description, resultAvatar.Description, TestContext.TestName);
+        }
+
+        //[TestMethod]
+        //public void Controller_Avatar_Update_Post_Valid_Should_Return_Index_Page()
+        //{
+        //    // Arrange
+        //    AvatarController controller = new AvatarController();
+
+        //    var data = new AvatarModel
+        //    {
+        //        Description = "description",
+        //        Id = "abc",
+        //        Name = "Name",
+        //        Uri = "picture"
+        //    };
+
+        //    // Act
+        //    var result = (RedirectToRouteResult)controller.Update(data);
+
+        //    // Assert
+        //    Assert.AreEqual("Index", result.RouteValues["action"], TestContext.TestName);
+        //    // No need to check the route, Assert.AreEqual("Avatar", result.RouteValues["route"], TestContext.TestName);
+
+        //    // Check that the item is Updated
+        //    var resultAvatar = AvatarBackend.Instance.Read("abc");
+        //    Assert.AreEqual(data.Id, resultAvatar.Id, TestContext.TestName);
+
+        //    // Reset the Avatars
+        //    AvatarBackend.Instance.Reset();
+
+        //}
+
+        [TestMethod]
+        public void Controller_Avatar_Update_Post_InValid_Should_Return_Error_Page()
+        {
+            /// <summary>
+            /// This Test calls the Update, but passes null data
+            /// The controller will return a redirect to the error home page
+            /// So the test needs to cast the return to a redirect, and then check that it got to the home error page
+            /// </summary>
+
+            // Arrange
+            AvatarController controller = new AvatarController();
+
+            // Act
+            var result = (RedirectToRouteResult)controller.Update((AvatarModel)null);
+
+            // Assert
+            Assert.AreEqual("Error", result.RouteValues["action"], TestContext.TestName);
+            Assert.AreEqual("Home", result.RouteValues["route"], TestContext.TestName);
+        }
+        #endregion UpdateRegion
+
 
         //    public ActionResult Update(string id = null)
         //    {
