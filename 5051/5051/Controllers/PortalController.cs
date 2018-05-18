d0ebe18d21797271b314c06323edcc2f2425312a
Michael Koenig <koenigm@seattleu.edu>
2018-05-18 06:04:33 -0700

    Added Portal Files, Added Shop Files

--- 5051/5051/Controllers/PortalController.cs
+++ 5051/5051/Controllers/PortalController.cs
@@ -8,8 +8,62 @@ namespace _5051.Controllers
 {
     public class PortalController : Controller
     {
+        /// <summary>
+        /// Index Page
+        /// </summary>
+        /// <returns></returns>
         // GET: Portal
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Student's Avatar page
+        /// </summary>
+        /// <returns></returns>
+        // GET: Portal
+        public ActionResult Avatar()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// The Group's House information
+        /// </summary>
+        /// <returns></returns>
+        // GET: Portal
+        public ActionResult Group()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///  My House
+        /// </summary>
+        /// <returns></returns>
+        // GET: Portal
+        public ActionResult House()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///  My Settings
+        /// </summary>
+        /// <returns></returns>
+        // GET: Portal
+        public ActionResult Setting()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// My Attendance Reports
+        /// </summary>
+        /// <returns></returns>
+        // GET: Portal
+        public ActionResult Report()
         {
             return View();
         }
