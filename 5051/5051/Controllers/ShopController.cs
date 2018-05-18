d5c26bca38e5e1caab1f6b0f55f82866d3a9177f
Michael Koenig <koenigm@seattleu.edu>
2018-05-18 08:15:29 -0700

    Added Shop Items to the project and updated controller

--- 5051/5051/Controllers/ShopController.cs
+++ 5051/5051/Controllers/ShopController.cs
@@ -8,8 +8,42 @@ namespace _5051.Controllers
 {
     public class ShopController : Controller
     {
+        /// <summary>
+        /// Index to the Shop
+        /// </summary>
+        /// <returns></returns>
         // GET: Shop
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// What to Buy at the store
+        /// </summary>
+        /// <returns></returns>
+        // GET: Buy
+        public ActionResult Buy()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Things on sale at the store
+        /// </summary>
+        /// <returns></returns>
+        // GET: Discounts
+        public ActionResult Discounts()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Unique items to get at the store
+        /// </summary>
+        /// <returns></returns>
+        // GET: Specials
+        public ActionResult Specials()
         {
             return View();
         }
