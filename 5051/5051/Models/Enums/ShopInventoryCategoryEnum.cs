56f3f2a2e9ff49331e4d25b4acb7eaed83a59bd0
Mike Koenig <mikekoenig@live.com>
2018-05-31 13:52:22 -0700

    Adding files for shop, and shop category enum

---
+++ 5051/5051/Models/Enums/ShopInventoryCategoryEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _5051.Models
+{
+    /// <summary>
+    /// Student Status Options
+    /// </summary>
+    public enum ShopInventoryCategoryEnum
+    {
+        Unknown = 0,
+
+        // Logged Out
+        Out = 1,
+
+        // Logged In
+        In = 2,
+
+        // Student on hold
+        Hold = 3
+    }
+}
