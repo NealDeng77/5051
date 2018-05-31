e3f0def1945bd9065f67391a33ee013ea0289ec8
Mike Koenig <mikekoenig@live.com>
2018-05-31 14:24:37 -0700

    Added Mock Shop Inventory Items

--- 5051/5051/Backend/ShopInventoryDataSourceMock.cs
+++ 5051/5051/Backend/ShopInventoryDataSourceMock.cs
@@ -145,17 +145,16 @@ namespace _5051.Backend
         private void DataSetDefault()
         {
             DataSetClear();
-            var count = 0;
-            Create(new ShopInventoryModel("ShopInventory" + count++.ToString() + ".png", "Police", "Happy Officer", 1));
-            Create(new ShopInventoryModel("ShopInventory" + count++.ToString() + ".png", "Kunoichi", "Ninja Lady", 2));
-            Create(new ShopInventoryModel("ShopInventory" + count++.ToString() + ".png", "Angry", "Angry, but happy", 1));
-            Create(new ShopInventoryModel("ShopInventory" + count++.ToString() + ".png", "Playfull", "Anyone want a ride?", 1));
-            Create(new ShopInventoryModel("ShopInventory" + count++.ToString() + ".png", "Pirate", "Where is my ship?", 2));
-            Create(new ShopInventoryModel("ShopInventory" + count++.ToString() + ".png", "Blue", "Having a Blue Day", 3));
-            Create(new ShopInventoryModel("ShopInventory" + count++.ToString() + ".png", "Pigtails", "Love my hair", 3));
-            Create(new ShopInventoryModel("ShopInventory" + count++.ToString() + ".png", "Ninja", "Taste my Katana", 2));
-            Create(new ShopInventoryModel("ShopInventory" + count++.ToString() + ".png", "Circus", "Swinging from the Trapeese", 4));
-            Create(new ShopInventoryModel("ShopInventory" + count++.ToString() + ".png", "Chef", "I love to cook", 4));
+            Create(new ShopInventoryModel("guitar.png", "Guitar", "6 strings and all",ShopInventoryCategoryEnum.Music,10 ));
+            Create(new ShopInventoryModel("saxophone.png", "Saxophone", "Smooth Sounds", ShopInventoryCategoryEnum.Music,10));
+            Create(new ShopInventoryModel("trumpet.png", "Trumpet", "Love the Horn", ShopInventoryCategoryEnum.Music,10));
+            Create(new ShopInventoryModel("violin.png", "Violin", "Sweet Melody", ShopInventoryCategoryEnum.Music,10));
+
+            Create(new ShopInventoryModel("radio.png", "radio", "Play some Tunes", ShopInventoryCategoryEnum.Entertainment,10));
+            Create(new ShopInventoryModel("telephone.png", "televion", "Talking with my buds", ShopInventoryCategoryEnum.Entertainment,10));
+            Create(new ShopInventoryModel("television.png", "Television", "I love lucy again?", ShopInventoryCategoryEnum.Entertainment,10));
+            Create(new ShopInventoryModel("headphone.png", "Headphone", "Just relazing", ShopInventoryCategoryEnum.Entertainment,10));
+            Create(new ShopInventoryModel("moviecamera.png", "Movie Camera", "Making Movies", ShopInventoryCategoryEnum.Entertainment,10));
         }
 
         /// <summary>
