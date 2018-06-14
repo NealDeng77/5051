702ea263e0b6e3708334edf98922591073e58444
Michael Koenig <koenigm@seattleu.edu>
2018-06-13 20:34:20 -0700

    Updated Name descriptions for the School Calendar Model

--- 5051/5051/Models/SchoolCalendarModel.cs
+++ 5051/5051/Models/SchoolCalendarModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using _5051.Models.Enums;
 
 namespace _5051.Models
@@ -11,41 +12,49 @@ namespace _5051.Models
         /// <summary>
         /// The Id for the Record
         /// </summary>
+        [Display(Name = "Id", Description = "Day Id")]
         public string Id { get; set; }
 
         /// <summary>
         /// The Date including year
         /// </summary>
+        [Display(Name = "Date", Description = "Date")]
         public DateTime Date { get; set; }
 
         /// <summary>
         /// The number of hours and minutes in the school day
         /// </summary>
+        [Display(Name = "Max Time", Description = "Hours for the Day")]
         public TimeSpan TimeMax { get; set; }
 
         /// <summary>
         /// The start of school time
         /// </summary>
+        [Display(Name = "Start Time", Description = "Start Time")]
         public TimeSpan TimeStart { get; set; }
 
         /// <summary>
         /// The End of school time  (full day, or part time)
         /// </summary>
+        [Display(Name = "End Time", Description = "End Time")]
         public TimeSpan TimeEnd { get; set; }
 
         /// <summary>
         /// School day Starts normal, early, or late (snow day)
         /// </summary>
+        [Display(Name = "Start Type", Description = "Type of Start")]
         public SchoolCalendarDismissalEnum DayStart { get; set; }
 
         /// <summary>
         /// School day Ends normal, early (wed early dismissal), or late
         /// </summary>
+        [Display(Name = "End Type", Description = "Type of End")]
         public SchoolCalendarDismissalEnum DayEnd { get; set; }
 
         /// <summary>
         /// Set to true if this record is modified from the default
         /// </summary>
+        [Display(Name = "Modified", Description = "Modified Record")]
         public bool Modified { get; set; }
 
         /// <summary>
