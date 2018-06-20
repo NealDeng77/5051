b3b54bcf1a489e9ba16e9cda78a57c7adbc3dfa3
Michael Koenig <koenigm@seattleu.edu>
2018-06-19 18:29:54 -0700

    Added Unit Test Project, with example Unit Test for SystemGlobals

---
+++ 5051/5051.UnitTests/Models/SystemGlobalsUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+
+namespace _5051.UnitTests.Models
+{
+    [TestClass]
+    public class SystemGlobalsUnitTests
+    {
+        public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Models_SystemGlobals_Default_Instantiate_Should_Pass()
+        {
+            // Arrange
+            var myModel = SystemGlobals.Instance;
+
+            // Act
+            var result = myModel.DataSourceValue;
+
+            // Assert
+            Assert.AreEqual(result, DataSourceEnum.Mock, TestContext.TestName);
+        }
+        #endregion Instantiate
+    }
+}
