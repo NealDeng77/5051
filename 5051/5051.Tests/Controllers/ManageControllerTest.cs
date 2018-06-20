f3301fb8796d8a871775a953979ae9e5dc29603e
Michael Koenig <koenigm@seattleu.edu>
2018-06-20 08:52:34 -0700

    Manage and Portal Controller Test

---
+++ 5051/5051.Tests/Controllers/ManageControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using _5051;
+using _5051.Controllers;
+
+namespace _5051.Tests.Controllers
+{
+    [TestClass]
+    public class ManageControllerTest
+    {
+        public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Controller_Manage_Instantiate_Default_Should_Pass()
+        {
+            // Arrange
+            var controller = new AdminController();
+
+            // Act
+            var result = controller.GetType();
+
+            // Assert
+            Assert.AreEqual(result, new AdminController().GetType(), TestContext.TestName);
+        }
+
+        #endregion Instantiate
+    }
+}
