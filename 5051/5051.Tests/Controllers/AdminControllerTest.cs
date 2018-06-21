7bdb07779083deb52723c045ff121f5bff0975dd
Tianhua Zhao <tzhaojustdoit@me.com>
2018-06-20 21:05:43 -0700

    Added Admin_Settings() test

--- 5051/5051.Tests/Controllers/AdminControllerTest.cs
+++ 5051/5051.Tests/Controllers/AdminControllerTest.cs
@@ -53,5 +53,18 @@ namespace _5051.Tests.Controllers
             Assert.IsNotNull(result, TestContext.TestName);
         }
 
+        [TestMethod]
+        public void Controller_Admin_Settings_Should_Pass()
+        {
+            // Arrange
+            AdminController controller = new AdminController();
+
+            // Act
+            ViewResult result = controller.Settings() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+
     }
 }
