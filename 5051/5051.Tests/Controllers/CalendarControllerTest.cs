85006f58a8c193086085135ec9cd9da1cfd61bef
karen2800 <Karen2800@hotmail.com>
2018-06-21 15:16:57 -0700

    added CalendarController.SetSchoolDay(string) test

--- 5051/5051.Tests/Controllers/CalendarControllerTest.cs
+++ 5051/5051.Tests/Controllers/CalendarControllerTest.cs
@@ -283,5 +283,57 @@ namespace _5051.Tests.Controllers
         }
 
         #endregion SetNoSchoolDayRegion
+
+        #region SetSchoolDayRegion
+
+        [TestMethod]
+        public void Controller_Calendar_SetSchoolDay_IdIsNull_ShouldReturnErrorPage()
+        {
+            // Arrange
+            CalendarController controller = new CalendarController();
+
+            // Act
+            var result = (RedirectToRouteResult)controller.SetSchoolDay(null);
+
+            // Assert
+            Assert.AreEqual("Error", result.RouteValues["action"], TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Controller_Calendar_SetSchoolDay_myDataIsNull_ShouldReturnErrorPage()
+        {
+            // Arrange
+            CalendarController controller = new CalendarController();
+
+            string id = DataSourceBackend.Instance.SchoolCalendarBackend.GetDefault().Id;
+
+            // Reset DataSourceBackend
+            DataSourceBackend.Instance.Reset();
+
+            // Act
+            var result = (RedirectToRouteResult)controller.SetSchoolDay(id);
+
+            // Assert
+            Assert.AreEqual("Error", result.RouteValues["action"], TestContext.TestName);
+            Assert.AreEqual("Home", result.RouteValues["controller"], TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Controller_Calendar_SetSchoolDay_ShouldReturnCalendarPage()
+        {
+            // Arrange
+            CalendarController controller = new CalendarController();
+
+            string id = DataSourceBackend.Instance.SchoolCalendarBackend.GetDefault().Id;
+
+            // Act
+            var result = (RedirectToRouteResult)controller.SetSchoolDay(id);
+
+            // Assert
+            Assert.AreEqual("Update", result.RouteValues["action"], TestContext.TestName);
+            Assert.AreEqual("Calendar", result.RouteValues["controller"], TestContext.TestName);
+        }
+
+        #endregion SetSchoolDayRegion
     }
 }
