76bf8d25a9c328f1664ae27077de1932f59c3d6b
Victor Chimenti <chimenti@seattleu.edu>
2018-06-22 10:34:56 -0700

    updating ShopController added regions for Discounts and Specials methods

--- 5051/5051.Tests/Controllers/ShopControllerTest.cs
+++ 5051/5051.Tests/Controllers/ShopControllerTest.cs
@@ -7,6 +7,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using _5051;
 using _5051.Controllers;
+using _5051.Models;
+using _5051.Backend;
 
 namespace _5051.Tests.Controllers
 {
@@ -30,5 +32,89 @@ namespace _5051.Tests.Controllers
         }
 
         #endregion Instantiate
+
+        [TestMethod]
+        public void Controller_Shop_Index_Default_Should_Pass()
+        {
+            // Arrange
+            ShopController controller = new ShopController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Controller_Shop_Buy_Post_ModelIsInvalid_Should_Pass()
+        {
+            // Arrange
+            ShopController controller = new ShopController();
+
+            ShopBuyViewModel data = new ShopBuyViewModel();
+
+            // Make ModelState Invalid
+            controller.ModelState.AddModelError("test", "test");
+
+            // Act
+            ViewResult result = controller.Buy(data) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Controller_Shop_Buy_Get_myDataIsNull_ShouldReturnErrorPage()
+        {
+            // Arrange
+            ShopController controller = new ShopController();
+
+            string id = DataSourceBackend.Instance.StudentBackend.GetDefault().Id;
+
+            // Reset DataSourceBackend
+            DataSourceBackend.Instance.Reset();
+
+            // Act
+            var result = (RedirectToRouteResult)controller.Buy(id);
+
+            // Assert
+            Assert.AreEqual("Error", result.RouteValues["action"], TestContext.TestName);
+            Assert.AreEqual("Home", result.RouteValues["controller"], TestContext.TestName);
+        }
+
+        #region DiscountsRegion
+
+        [TestMethod]
+        public void Controller_Shop_Discounts_Default_Should_Pass()
+        {
+            // Arrange
+            ShopController controller = new ShopController();
+
+            // Act
+            ViewResult result = controller.Discounts() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+
+        #endregion DiscountsRegion
+
+        #region SpecialsRegion
+
+        [TestMethod]
+        public void Controller_Shop_Specials_Default_Should_Pass()
+        {
+            // Arrange
+            ShopController controller = new ShopController();
+
+            // Act
+            ViewResult result = controller.Specials() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+
+        #endregion SpecialsRegion
     }
 }
