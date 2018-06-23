7ce5325d20b4e80006d9c603b6db47fe128908d9
Michael Koenig <koenigm@seattleu.edu>
2018-06-23 08:29:25 -0700

    Adding StudentView Model Unit Test to project

---
+++ 5051/5051.UnitTests/Models/StudentViewModelUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+
+namespace _5051.UnitTests.Models
+{
+    [TestClass]
+    public class StudentViewModelUnitTests
+    {
+        public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Models_StudentViewModel_Default_Instantiate_Should_Pass()
+        {
+
+            // Act
+            var result = new StudentViewModel();
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+
+        #endregion Instantiate
+
+    }
+}
