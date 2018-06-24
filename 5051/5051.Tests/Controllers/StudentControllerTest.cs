0624f6af0fb0dea85ec0b1aad87e1a9ef565d139
karen2800 <Karen2800@hotmail.com>
2018-06-24 11:49:35 -0700

    added StudentController.Create(StudentModel) test

--- 5051/5051.Tests/Controllers/StudentControllerTest.cs
+++ 5051/5051.Tests/Controllers/StudentControllerTest.cs
@@ -117,5 +117,79 @@ namespace _5051.Tests.Controllers
 
         #endregion CreateRegion
 
+        #region CreatePostRegion
+
+        [TestMethod]
+        public void Controller_Student_Create_Post_ModelIsInvalid_Should_Pass()
+        {
+            // Arrange
+            StudentController controller = new StudentController();
+
+            StudentModel data = new StudentModel();
+
+            // Make ModelState Invalid
+            controller.ModelState.AddModelError("test", "test");
+
+            // Act
+            ViewResult result = controller.Create(data) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Controller_Student_Create_Post_DataIsNull_ShouldReturnErrorPage()
+        {
+            // Arrange
+            StudentController controller = new StudentController();
+
+            // Act
+            var result = (RedirectToRouteResult)controller.Create((StudentModel)null);
+
+            // Assert
+            Assert.AreEqual("Error", result.RouteValues["action"], TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Controller_Student_Create_Post_IdIsNullOrEmpty_ShouldReturnErrorPage()
+        {
+            // Arrange
+            StudentController controller = new StudentController();
+
+            StudentModel dataNull = new StudentModel();
+            StudentModel dataEmpty = new StudentModel();
+
+            // Make data.Id = null
+            dataNull.Id = null;
+
+            // Make data.Id empty
+            dataEmpty.Id = "";
+
+            // Act
+            var resultNull = (ViewResult)controller.Create(dataNull);
+            var resultEmpty = (ViewResult)controller.Create(dataEmpty);
+
+            // Assert
+            Assert.IsNotNull(resultNull, TestContext.TestName);
+            Assert.IsNotNull(resultEmpty, TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Controller_Student_Create_Post_Default_Should_Pass()
+        {
+            // Arrange
+            StudentController controller = new StudentController();
+
+            StudentModel data = DataSourceBackend.Instance.StudentBackend.GetDefault();
+
+            // Act
+            var result = (RedirectToRouteResult)controller.Create(data);
+
+            // Assert
+            Assert.AreEqual("Index", result.RouteValues["action"], TestContext.TestName);
+        }
+
+        #endregion CreatePostRegion
+
     }
 }
