c2a258c7b7ffca8d15c220bf65513e92ee8bf2c8
Mike Koenig <mikekoenig@live.com>
2018-06-23 18:34:40 -0700

    Refactored StudentDisplayViewModel from StudentModel, added Model Tests.  100%

---
+++ 5051/5051/Models/StudentDisplayViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+using _5051.Backend;
+
+namespace _5051.Models
+{
+    /// <summary>
+    /// For the Index View, add the Avatar URI to the Student, so it shows the student with the picture
+    /// </summary>
+    public class StudentDisplayViewModel : StudentModel
+    {
+        /// <summary>
+        /// The path to the local image for the avatar
+        /// </summary>
+        [Display(Name = "Avatar Picture", Description = "Avatar Picture to Show")]
+        public string AvatarUri { get; set; }
+
+        /// <summary>
+        /// Display name for the Avatar on the student information (Friendly Police etc.)
+        /// </summary>
+        [Display(Name = "Avatar Name", Description = "Avatar Name")]
+        public string AvatarName { get; set; }
+
+        /// <summary>
+        /// Description of the Avatar to show on the student information
+        /// </summary>
+        [Display(Name = "Avatar Description", Description = "Avatar Description")]
+        public string AvatarDescription { get; set; }
+
+        /// <summary>
+        /// DateTime of last transaction recorded, used for login and logout
+        /// </summary>
+        [Display(Name = "Date", Description = "Date and Time")]
+        public DateTime LastDateTime { get; set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public StudentDisplayViewModel() { }
+
+        /// <summary>
+        /// Creates a Student Display View Model from a Student Model
+        /// </summary>
+        /// <param name="data">The Student Model to create</param>
+        public StudentDisplayViewModel(StudentModel data)
+        {
+            if (data == null)
+            {
+                // Nothing to convert
+                return;
+            }
+
+            Id = data.Id;
+            Name = data.Name;
+            Tokens = data.Tokens;
+            AvatarLevel = data.AvatarLevel;
+            AvatarId = data.AvatarId;
+            Status = data.Status;
+            ExperiencePoints = data.ExperiencePoints;
+            Password = data.Password;
+            Inventory = data.Inventory;
+            Attendance = data.Attendance;
+
+            var myDataAvatar = AvatarBackend.Instance.Read(AvatarId);
+            if (myDataAvatar == null)
+            {
+                // Nothing to convert
+                return;
+            }
+
+            AvatarName = myDataAvatar.Name;
+            AvatarDescription = myDataAvatar.Description;
+            AvatarUri = myDataAvatar.Uri;
+        }
+    }
+}
--- 5051/5051/Models/StudentModel.cs
+++ 5051/5051/Models/StudentModel.cs
@@ -49,7 +49,7 @@ namespace _5051.Models
         /// </summary>
         [Display(Name = "XP", Description = "Experience Points Earned")]
         [Required(ErrorMessage = "XP is required")]
-        public int ExperiencePoints{ get; set; }
+        public int ExperiencePoints { get; set; }
 
         /// <summary>
         /// The number of Tokens the student has, tokens are used in the store, and also to level up
@@ -70,7 +70,7 @@ namespace _5051.Models
         /// </summary>
         [Display(Name = "Password", Description = "Student Password")]
         [PasswordPropertyText]
-        public string Password{ get; set; }
+        public string Password { get; set; }
 
         /// <summary>
         /// The inventory list for the student
@@ -80,7 +80,7 @@ namespace _5051.Models
         /// <summary>
         /// The Attendance list for the student
         /// </summary>
-        public List<AttendanceModel> Attendance{ get; set; }
+        public List<AttendanceModel> Attendance { get; set; }
 
 
         /// <summary>
@@ -167,76 +167,6 @@ namespace _5051.Models
             Attendance = data.Attendance;
 
             return true;
-        }
-    }
-
-    /// <summary>
-    /// For the Index View, add the Avatar URI to the Student, so it shows the student with the picture
-    /// </summary>
-    public class StudentDisplayViewModel : StudentModel
-    {
-        /// <summary>
-        /// The path to the local image for the avatar
-        /// </summary>
-        [Display(Name = "Avatar Picture", Description = "Avatar Picture to Show")]
-        public string AvatarUri { get; set; }
-
-        /// <summary>
-        /// Display name for the Avatar on the student information (Friendly Police etc.)
-        /// </summary>
-        [Display(Name = "Avatar Name", Description = "Avatar Name")]
-        public string AvatarName { get; set; }
-
-        /// <summary>
-        /// Description of the Avatar to show on the student information
-        /// </summary>
-        [Display(Name = "Avatar Description", Description = "Avatar Description")]
-        public string AvatarDescription { get; set; }
-
-        /// <summary>
-        /// DateTime of last transaction recorded, used for login and logout
-        /// </summary>
-        [Display(Name = "Date", Description = "Date and Time")]
-        public DateTime LastDateTime { get; set; }
-
-        /// <summary>
-        /// Default constructor
-        /// </summary>
-        public StudentDisplayViewModel() { }
-
-        /// <summary>
-        /// Creates a Student Display View Model from a Student Model
-        /// </summary>
-        /// <param name="data">The Student Model to create</param>
-        public StudentDisplayViewModel(StudentModel data)
-        {
-            if (data == null)
-            {
-                // Nothing to convert
-                return;
-            }
-
-            Id = data.Id;
-            Name = data.Name;
-            Tokens = data.Tokens;
-            AvatarLevel = data.AvatarLevel;
-            AvatarId = data.AvatarId;
-            Status = data.Status;
-            ExperiencePoints = data.ExperiencePoints;
-            Password = data.Password;
-            Inventory = data.Inventory;
-            Attendance = data.Attendance;
-
-            var myDataAvatar = AvatarBackend.Instance.Read(AvatarId);
-            if (myDataAvatar == null)
-            {
-                // Nothing to convert
-                return;
-            }
-
-            AvatarName = myDataAvatar.Name;
-            AvatarDescription = myDataAvatar.Description;
-            AvatarUri = myDataAvatar.Uri;
         }
     }
 }
