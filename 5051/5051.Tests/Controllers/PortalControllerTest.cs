b6c5a812cd56e86c4625184928b9bc91546de1f6
Lena Wang <lena@symphoni.cc>
2018-06-24 13:05:10 -0700

    Updated PortalController.Index(string) test. please sync

--- 5051/5051.Tests/Controllers/PortalControllerTest.cs
+++ 5051/5051.Tests/Controllers/PortalControllerTest.cs
@@ -86,7 +86,7 @@ namespace _5051.Tests.Controllers
         }
 
         [TestMethod]
-        public void Controller_Protal_Index_IDValid_Should_Pass()
+        public void Controller_Protal_Index_IDValid_ShouldPass()
         {
             // Arrange
             PortalController controller = new PortalController();
