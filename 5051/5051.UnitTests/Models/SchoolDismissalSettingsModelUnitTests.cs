f75153b5da86a1a61166fc993070774814f92e31
Mike Koenig <mikekoenig@live.com>
2018-06-25 22:47:42 -0700

    UT for Update in SchoolDismissalSettings Model

--- 5051/5051.UnitTests/Models/SchoolDismissalSettingsModelUnitTests.cs
+++ 5051/5051.UnitTests/Models/SchoolDismissalSettingsModelUnitTests.cs
@@ -82,5 +82,65 @@ namespace _5051.UnitTests.Models
         }
 
         #endregion Instantiate
+
+        #region Update
+
+        [TestMethod]
+        public void Models_SchoolDismissalSettingsModel_Update_Invalid_Should_Fail()
+        {
+            // Arrange
+            var data = new SchoolDismissalSettingsModel();
+            var expect = data.EndNormal;
+
+            // Act
+            data.Update(null);
+            var result = data.EndNormal;
+
+            // Assert
+            Assert.AreEqual(expect, result, TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Models_SchoolDismissalSettingsModel_Update_Valid_Should_Pass()
+        {
+
+            // Arrange
+            var data = new SchoolDismissalSettingsModel();
+            data.StartNormal = TimeSpan.Parse("1:11");
+            data.StartEarly = TimeSpan.Parse("1:12");
+            data.StartLate = TimeSpan.Parse("1:13");
+
+            data.EndNormal = TimeSpan.Parse("2:11");
+            data.EndEarly = TimeSpan.Parse("2:12");
+            data.EndLate = TimeSpan.Parse("2:13");
+
+
+            var Year = DateTime.UtcNow.Year;
+            if (DateTime.UtcNow.Month > 1)
+            {
+                Year--;
+            }
+            data.DayFirst = DateTime.Parse("09/01/" + Year.ToString());
+            data.DayLast = DateTime.Parse("06/30/" + (Year + 1).ToString());
+
+            // Act
+            var result = new SchoolDismissalSettingsModel();
+            result.Update(data);
+
+            // Assert
+
+            // Checking Each value updated
+            Assert.AreEqual(data.StartNormal, result.StartNormal, "StartNormal" + TestContext.TestName);
+            Assert.AreEqual(data.StartEarly, result.StartEarly, "StartEarly" + TestContext.TestName);
+            Assert.AreEqual(data.StartLate, result.StartLate, "StartLate" + TestContext.TestName);
+
+            Assert.AreEqual(data.EndNormal, result.EndNormal, "EndNormal" + TestContext.TestName);
+            Assert.AreEqual(data.EndEarly, result.EndEarly, "EndEarly" + TestContext.TestName);
+            Assert.AreEqual(data.EndLate, result.EndLate, "EndLate" + TestContext.TestName);
+
+            Assert.AreEqual(data.DayFirst, result.DayFirst, "DayFirst" + TestContext.TestName);
+            Assert.AreEqual(data.DayLast, result.DayLast, "DayLast" + TestContext.TestName);
+        }
+        #endregion Update
     }
 }
