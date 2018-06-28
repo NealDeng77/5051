e9ff08ed81a78a7fb3a87a90762043633a119ac2
Mike Koenig <mikekoenig@live.com>
2018-06-28 07:53:51 -0700

    Added UT Avatar Datasource Mock file

---
+++ 5051/5051.UnitTests/Backend/AvatarDataSourceMockUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+using _5051.Backend;
+
+namespace _5051.UnitTests.Models
+{
+    [TestClass]
+    public class AvatarDataSourceMockUnitTests
+    {
+        public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Models_AvatarDataSourceMock_Default_Instantiate_Should_Pass()
+        {
+            // Arrange
+            var backend = AvatarDataSourceMock.Instance;
+
+            //var expect = backend;
+
+            // Act
+            var result = backend;
+
+            //Reset
+            backend.Reset();
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+
+        #endregion Instantiate
+    }
+}
