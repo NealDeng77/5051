c0211261b3be89f54ae0a20ac105cc3d19487931
Troy Osland <oslandt@seattleu.edu>
2018-06-29 00:57:28 -0700

    added test file for AvatarBackend

---
+++ 5051/5051.UnitTests/Backend/AvatarBackendUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+using _5051.Backend;
+
+namespace _5051.UnitTests.Models
+{
+    [TestClass]
+    public class AvatarBackendUnitTests
+    {
+        public TestContext TestContext { get; set; }
+
+        
+
+    }
+}
