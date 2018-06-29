5c23b4cfa4246246e40d5b8c9b0f15c126ec0593
Wonseok Seo <seow@seattleu.edu>
2018-06-29 15:43:23 -0700

    Added ManageController.MangeController(ApplicationUserManager, ApplicationSignInManager) test

--- 5051/5051.Tests/Controllers/ManageControllerTest.cs
+++ 5051/5051.Tests/Controllers/ManageControllerTest.cs
@@ -7,6 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using _5051;
 using _5051.Controllers;
+using _5051.Models;
 
 namespace _5051.Tests.Controllers
 {
@@ -20,15 +21,27 @@ namespace _5051.Tests.Controllers
         public void Controller_Manage_Instantiate_Default_Should_Pass()
         {
             // Arrange
-            var controller = new AdminController();
+            var controller = new ManageController();
 
             // Act
             var result = controller.GetType();
 
             // Assert
-            Assert.AreEqual(result, new AdminController().GetType(), TestContext.TestName);
+            Assert.AreEqual(result, new ManageController().GetType(), TestContext.TestName);
         }
 
+        [TestMethod]
+        public void Controller_Manage_Instantiate_Default_Application_User_Manager_And_Signin_Manager_Should_Pass()
+        {
+            // Arrage
+            var controller = new ManageController((ApplicationUserManager)null, (ApplicationSignInManager)null);
+
+            // Act
+            var result = controller.GetType();
+
+            // Assert
+            Assert.AreEqual(result, new ManageController().GetType(), TestContext.TestName);
+        }
         #endregion Instantiate
     }
 }
