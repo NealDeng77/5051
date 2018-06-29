cbc402ac009f4a469af983cc2f9d29050bea5049
Mike Koenig <mikekoenig@live.com>
2018-06-29 15:19:02 -0700

    Added Emotion Status Enum to the Project, and a Unit Test for it.

---
+++ 5051/5051.UnitTests/Models/Enums/EmotionStatusEnumUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+using _5051.Models.Enums;
+
+namespace _5051.UnitTests.Models
+{
+    [TestClass]
+    public class EmotionStatusEnumUnitTests
+    {
+        public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Models_EmotionStatusEnumUnitTests_Values_Should_Pass()
+        {
+
+            // Assert
+            Assert.AreEqual(5, (int)EmotionStatusEnum.VeryHappy, TestContext.TestName);
+            Assert.AreEqual(4, (int)EmotionStatusEnum.Happy, TestContext.TestName);
+            Assert.AreEqual(3, (int)EmotionStatusEnum.Neutral, TestContext.TestName);
+            Assert.AreEqual(2, (int)EmotionStatusEnum.Sad, TestContext.TestName);
+            Assert.AreEqual(1, (int)EmotionStatusEnum.VerySad, TestContext.TestName);
+        }
+        #endregion Instantiate
+    }
+}
---
+++ 5051/5051/Models/Enums/EmotionStatusEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace _5051.Models
+{
+    /// <summary>
+    /// Emotion Status
+    /// </summary>
+    public enum EmotionStatusEnum
+    {
+        VeryHappy = 5,
+        Happy = 4,
+        Neutral = 3,
+        Sad = 2,
+        VerySad = 1,
+    }
+}
