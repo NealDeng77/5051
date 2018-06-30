0277f8035328bd124cb6d203b778e52e795df5c4
Lena Wang <lena@symphoni.cc>
2018-06-30 00:59:12 -0700

    added ShopInventory.Read(string) test

--- 5051/5051.Tests/Controllers/ShopInventoryControllerTest.cs
+++ 5051/5051.Tests/Controllers/ShopInventoryControllerTest.cs
@@ -8,6 +8,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using _5051;
 using _5051.Controllers;
 using _5051.Models;
+using _5051.Backend;
 
 namespace _5051.Tests.Controllers
 {
@@ -32,5 +33,43 @@ namespace _5051.Tests.Controllers
             Assert.IsNotNull(resultShopInventoryViewModel, TestContext.TestName);
         }
         #endregion IndexRegion
+
+        #region ReadRegion
+        [TestMethod]
+        public void Controller_ShopInventory_Read_IDIsNull_Should_Pass()
+        {
+            // Arrange
+            ShopInventoryController controller = new ShopInventoryController();
+            string id = null;
+
+            // Act
+            ViewResult result = controller.Read(id) as ViewResult;
+
+            var resultShopInventoryModel = result.Model as ShopInventoryModel;
+
+            // Assert
+            Assert.IsNotNull(resultShopInventoryModel, TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Controller_ShopInventory_Read_IDValid_Should_Pass()
+        {
+            // Arrange
+            ShopInventoryController controller = new ShopInventoryController();
+            ShopInventoryModel data = new ShopInventoryModel();
+            string id = Backend.ShopInventoryBackend.Instance.Create(data).Id;
+
+            // Act
+            ViewResult result = controller.Read(id) as ViewResult;
+
+            var resultShopInventoryModel = result.Model as ShopInventoryModel;
+
+            // Reset StudentBackend
+            ShopInventoryBackend.Instance.Reset();
+
+            // Assert
+            Assert.IsNotNull(resultShopInventoryModel, TestContext.TestName);
+        }
+        #endregion ReadRegion
     }
 }
