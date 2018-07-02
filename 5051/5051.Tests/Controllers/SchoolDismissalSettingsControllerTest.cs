253db67b80e034571b4553f26f7c800949df675f
Mike Koenig <mikekoenig@live.com>
2018-07-01 17:38:21 -0700

    Added Unit Test for School Dismissal Settings Update

--- 5051/5051.Tests/Controllers/SchoolDismissalSettingsControllerTest.cs
+++ 5051/5051.Tests/Controllers/SchoolDismissalSettingsControllerTest.cs
@@ -106,6 +106,23 @@ namespace _5051.Tests.Controllers
             Assert.AreEqual("Error", result.RouteValues["action"], TestContext.TestName);
             Assert.AreEqual("Home", result.RouteValues["controller"], TestContext.TestName);
         }
+
+        [TestMethod]
+        public void Controller_SchoolDismissalSettings_Get_Update_Invalid_Id_Null_Should_Return_Error_Page()
+        {
+            // Arrange
+            var controller = new SchoolDismissalSettingsController();
+            string id = null;
+
+            // Act
+            var result = (ViewResult)controller.Update(id);
+            var resultModel = (SchoolDismissalSettingsModel)result.Model;
+
+            var expect = SchoolDismissalSettingsBackend.Instance.GetDefault();
+
+            // Assert
+            Assert.AreEqual(expect.Id, resultModel.Id, TestContext.TestName);
+        }
         #endregion GetUpdateRegion
 
         #region PostUpdateRegion
