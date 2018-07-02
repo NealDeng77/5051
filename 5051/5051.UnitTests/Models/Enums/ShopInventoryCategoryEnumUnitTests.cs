34d0cfcb000f44225f24139e7480c4789d04463c
Mike Koenig <mikekoenig@live.com>
2018-07-01 22:08:49 -0700

    Added UT for ShopInventoryCategoryEnum

---
+++ 5051/5051.UnitTests/Models/Enums/ShopInventoryCategoryEnumUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+
+namespace _5051.UnitTests.Models
+{
+    [TestClass]
+    public class ShopInventoryCategoryEnumUnitTests
+    {
+        public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Models_ShopInventoryCategoryEnumUnitTests_Values_Should_Pass()
+        {
+            // Assert
+
+            // Make sure there are no additional values
+            var enumCount = ShopInventoryCategoryEnum.GetNames(typeof(ShopInventoryCategoryEnum)).Length;
+            Assert.AreEqual(3, enumCount, TestContext.TestName);
+
+            // Check each value against their expected value.
+            Assert.AreEqual(0, (int)ShopInventoryCategoryEnum.Unknown, TestContext.TestName);
+            Assert.AreEqual(1, (int)ShopInventoryCategoryEnum.Music, TestContext.TestName);
+            Assert.AreEqual(2, (int)ShopInventoryCategoryEnum.Entertainment, TestContext.TestName);
+        }
+        #endregion Instantiate
+    }
+}
