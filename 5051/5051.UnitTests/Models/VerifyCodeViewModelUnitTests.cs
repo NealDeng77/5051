9540def67f2496ac247b79c6fbac5be6d129b1b5
Troy Osland <oslandt@seattleu.edu>
2018-07-02 16:52:36 -0700

    added UT for VerifyCodeViewModel

---
+++ 5051/5051.UnitTests/Models/VerifyCodeViewModelUnitTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+
+namespace _5051.UnitTests.Models
+{
+    [TestClass]
+    public class VerifyCodeViewModelUnitTests
+    {
+        public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Models_VerifyCodeViewModel_Default_Instantiate_Should_Pass()
+        {
+            //arrange
+            var test = new VerifyCodeViewModel();
+            var expectProvider = "Good Provider";
+            var expectCode = "Good Code";
+            var expectReturnUrl = "www.deliciousurl.gov";
+            var expectRememberBrower = true;
+            var expectRememberMe = true;
+
+            //act
+            var resultProvider = test.Provider;
+            var resultCode = test.Code;
+            var resultReturnUrl = test.ReturnUrl;
+            var resultRememberBrowser = test.RememberBrowser;
+            var resultRememberMe = test.RememberMe;
+
+            test.Provider = expectProvider;
+            test.Code = expectCode;
+            test.ReturnUrl = expectReturnUrl;
+            test.RememberBrowser = expectRememberBrower;
+            test.RememberMe = expectRememberMe;
+
+            var setResultProvider = test.Provider;
+            var setResultCode = test.Code;
+            var setResultReturnUrl = test.ReturnUrl;
+            var setResultRememberBrowser = test.RememberBrowser;
+            var setResultRememberMe = test.RememberMe;
+
+            //assert
+            Assert.IsNull(resultProvider, TestContext.TestName);
+            Assert.IsNull(resultCode, TestContext.TestName);
+            Assert.IsNull(resultReturnUrl, TestContext.TestName);
+            Assert.IsFalse(resultRememberBrowser, TestContext.TestName);
+            Assert.IsFalse(resultRememberMe, TestContext.TestName);
+
+            Assert.AreEqual(expectProvider, setResultProvider, TestContext.TestName);
+            Assert.AreEqual(expectCode, setResultCode, TestContext.TestName);
+            Assert.AreEqual(expectReturnUrl, setResultReturnUrl, TestContext.TestName);
+            Assert.AreEqual(expectRememberBrower, setResultRememberBrowser, TestContext.TestName);
+            Assert.AreEqual(expectRememberMe, setResultRememberMe, TestContext.TestName);
+        }
+        #endregion Instantiate
+
+    }
+}
