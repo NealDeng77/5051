049068cc761a1fe4c6a59127204a3b60e3689f3d
Troy Osland <oslandt@seattleu.edu>
2018-07-02 17:07:25 -0700

    added UT for SetPasswordViewModel

---
+++ 5051/5051.UnitTests/Models/SetPasswordViewModelUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+
+namespace _5051.UnitTests.Models
+{
+	[TestClass]
+    public class SetPasswordViewModelUnitTests
+    {
+		public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Models_SetPasswordViewModel_Default_Instantiate_Should_Pass()
+        {
+            //arrange
+            var test = new SetPasswordViewModel();
+            var expectNew = "passWORD23";
+            var expectConfirm = expectNew;
+
+            //act
+            var resultNew = test.NewPassword;
+            var resultConfirm = test.ConfirmPassword;
+
+            test.NewPassword = expectNew;
+            test.ConfirmPassword = expectConfirm;
+
+            var setResultNew = test.NewPassword;
+            var setResultConfirm = test.ConfirmPassword;
+
+            //assert
+            Assert.IsNull(resultNew, TestContext.TestName);
+            Assert.IsNull(resultConfirm, TestContext.TestName);
+
+            Assert.AreEqual(expectNew, setResultNew, TestContext.TestName);
+            Assert.AreEqual(expectConfirm, setResultConfirm, TestContext.TestName);
+        }
+        #endregion Instantiate
+    }
+}
