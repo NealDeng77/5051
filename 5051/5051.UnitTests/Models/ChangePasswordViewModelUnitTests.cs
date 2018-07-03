0fa61224850e5e84095443e1473a94f0322723e2
aguiarj1 <aguiarj1@seattleu.edu>
2018-07-03 14:04:08 -0700

    UT for ChangePasswordViewModel

---
+++ 5051/5051.UnitTests/Models/ChangePasswordViewModelUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+
+namespace _5051.UnitTests.Models
+{
+    class ChangePasswordViewModelUnitTests
+    {
+        public TestContext TestContext { get; set; }
+    }
+}
