8fa1e173d18f8e3781345e7f991ee6a09cdbb3f1
Troy Osland <oslandt@seattleu.edu>
2018-07-08 12:32:55 -0700

    Added a UT for AttendanceReportViewModel; get and set

--- 5051/5051.UnitTests/Models/AttendanceReportViewModelUnitTests.cs
+++ 5051/5051.UnitTests/Models/AttendanceReportViewModelUnitTests.cs
@@ -25,6 +25,52 @@ namespace _5051.UnitTests.Models
             Assert.IsNotNull(result, TestContext.TestName);
         }
 
+        [TestMethod]
+        public void Models_AttendanceReportViewModel_Default_Instantiate_Get_Set_Should_Pass()
+        {
+            //arrange
+            var result = new AttendanceReportViewModel();
+            var expectDate = DateTime.Today;
+            var expectIsSchoolDay = false;
+            var expectTimeIn = DateTime.UtcNow;
+            var expectTimeOut = DateTime.UtcNow;
+            var expectTotalHours = TimeSpan.Zero;
+            var expectTotalHoursExpected = TimeSpan.MaxValue;
+            var expectHoursAttended = TimeSpan.Zero;
+            var expectHoursExpected = TimeSpan.MaxValue;
+            var expectPercentAttended = 10;
+            var expectAttendanceStatus = _5051.Models.Enums.AttendanceStatusEnum.Present;
+            var expectCheckInStatus = _5051.Models.Enums.CheckInStatusEnum.ArriveOnTime;
+            var expectCheckOutstatus = _5051.Models.Enums.CheckOutStatusEnum.DoneEarly;
+
+            // Act
+            result.Date = expectDate;
+            result.IsSchoolDay = expectIsSchoolDay;
+            result.TimeIn = expectTimeIn;
+            result.TimeOut = expectTimeOut;
+            result.TotalHours = expectTotalHours;
+            result.TotalHoursExpected = expectTotalHoursExpected;
+            result.HoursAttended = expectHoursAttended;
+            result.HoursExpected = expectHoursExpected;
+            result.PercentAttended = expectPercentAttended;
+            result.AttendanceStatus = expectAttendanceStatus;
+            result.CheckInStatus = expectCheckInStatus;
+            result.CheckOutStatus = expectCheckOutstatus;
+
+            // Assert
+            Assert.AreEqual(expectDate, result.Date, TestContext.TestName);
+            Assert.AreEqual(expectIsSchoolDay, result.IsSchoolDay, TestContext.TestName);
+            Assert.AreEqual(expectTimeIn, result.TimeIn, TestContext.TestName);
+            Assert.AreEqual(expectTimeOut, result.TimeOut, TestContext.TestName);
+            Assert.AreEqual(expectTotalHours, result.TotalHours, TestContext.TestName);
+            Assert.AreEqual(expectTotalHoursExpected, result.TotalHoursExpected, TestContext.TestName);
+            Assert.AreEqual(expectHoursAttended, result.HoursAttended, TestContext.TestName);
+            Assert.AreEqual(expectHoursExpected, result.HoursExpected, TestContext.TestName);
+            Assert.AreEqual(expectPercentAttended, result.PercentAttended, TestContext.TestName);
+            Assert.AreEqual(expectAttendanceStatus, result.AttendanceStatus, TestContext.TestName);
+            Assert.AreEqual(expectCheckInStatus, result.CheckInStatus, TestContext.TestName);
+            Assert.AreEqual(expectCheckOutstatus, result.CheckOutStatus, TestContext.TestName);
+        }
         #endregion Instantiate
     }
 }
