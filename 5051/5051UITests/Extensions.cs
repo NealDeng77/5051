21dd075ad85ddf2837f501f277c7dc5f2629f4c7
Troy Osland <oslandt@seattleu.edu>
2018-07-10 17:55:12 -0700

    Added the beginnings of UI testing to the project

---
+++ 5051/5051UITests/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace _5051UITests
+{
+    public class Extensions
+    {
+        public const string LocalUrl = "http://localhost:59052/";
+        public const string BaseUrl = LocalUrl;
+
+        public static string ChromeDriverLocation = "../../ChromeDriver";
+
+        public const int WaitTime = 10;
+
+        public static bool ValidatePageTransition(IWebDriver driver, string controller, string action, string data = null)
+        {
+            driver.FindElement(By.Id("Page-Done"));
+            driver.FindElement(By.Id("Area--Done"));
+            driver.FindElement(By.Id("Controller-" + controller + "-Done"));
+            driver.FindElement(By.Id("View-" + action + "-Done"));
+
+            return true;
+        }
+
+        public static bool NavigateToPage(IWebDriver driver, string controller, string action, string data = null)
+        {
+
+            //navigate to the desired page
+            driver.Navigate().GoToUrl(Extensions.BaseUrl + "/" + controller + "/" + action + "/" + data);
+
+            //check that page is the right page
+            ValidatePageTransition(driver, controller, action);
+
+            return true;
+        }
+    }
+}
