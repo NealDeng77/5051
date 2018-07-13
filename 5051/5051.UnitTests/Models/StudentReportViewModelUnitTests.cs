0e756f8ff1c9f560b195737dc6b03745ddf5d1dc
Troy Osland <oslandt@seattleu.edu>
2018-07-12 21:31:24 -0700

    updated StudentReportViewModel UTs, back at 100%

--- 5051/5051.UnitTests/Models/StudentReportViewModelUnitTests.cs
+++ 5051/5051.UnitTests/Models/StudentReportViewModelUnitTests.cs
@@ -14,36 +14,45 @@ namespace _5051.UnitTests.Models
         public TestContext TestContext { get; set; }
 
         #region Instantiate
-        //[TestMethod]
-        //public void Models_StudentReportViewModel_Default_Instantiate_Should_Pass()
-        //{
+        [TestMethod]
+        public void Models_StudentReportViewModel_Default_Instantiate_Should_Pass()
+        {
 
-        //    // Act
-        //    var result = new StudentReportViewModel();
+            // Act
+            var result = new StudentReportViewModel();
 
-        //    // Assert
-        //    Assert.IsNotNull(result, TestContext.TestName);
-        //}
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
 
-        //[TestMethod]
-        //public void Models_StudentReportViewModel_Default_Instantiate_Get_Set_Data_Should_Pass()
-        //{
-        //    //arrange
-        //    var result = new StudentReportViewModel();
-        //    var expectStudent = new StudentModel();
-        //    var expectDateStart = new DateTime();
-        //    var expectDateEnd = DateTime.UtcNow;
+        [TestMethod]
+        public void Models_StudentReportViewModel_Default_Instantiate_Get_Set_Data_Should_Pass()
+        {
+            //arrange
+            var result = new StudentReportViewModel();
+            var expectStudentId = "GoodStudentID";
+            var expectYear = 2018;
+            var expectMonth = 4;
+            var expectStudent = new StudentModel();
+            var expectDateStart = new DateTime();
+            var expectDateEnd = DateTime.UtcNow;
 
-        //    // Act
-        //    result.Student = expectStudent;
-        //    result.DateStart = expectDateStart;
-        //    result.DateEnd = expectDateEnd;
+            // Act
+            result.StudentId = expectStudentId;
+            result.Year = expectYear;
+            result.Month = expectMonth;
+            result.Student = expectStudent;
+            result.DateStart = expectDateStart;
+            result.DateEnd = expectDateEnd;
 
-        //    // Assert
-        //    Assert.AreEqual(expectStudent, result.Student, TestContext.TestName);
-        //    Assert.AreEqual(expectDateStart, result.DateStart, TestContext.TestName);
-        //    Assert.AreEqual(expectDateEnd, result.DateEnd, TestContext.TestName);
-        //}
+            // Assert
+            Assert.AreEqual(expectStudentId, result.StudentId, TestContext.TestName);
+            Assert.AreEqual(expectYear, result.Year, TestContext.TestName);
+            Assert.AreEqual(expectMonth, result.Month, TestContext.TestName);
+            Assert.AreEqual(expectStudent, result.Student, TestContext.TestName);
+            Assert.AreEqual(expectDateStart, result.DateStart, TestContext.TestName);
+            Assert.AreEqual(expectDateEnd, result.DateEnd, TestContext.TestName);
+        }
         #endregion Instantiate
     }
 }
