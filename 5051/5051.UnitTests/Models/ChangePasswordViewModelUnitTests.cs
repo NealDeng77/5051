dc4062fc1f4ea0ce67a7d1a6c3385f7c2d3132d6
Mike Koenig <mikekoenig@live.com>
2018-07-15 17:43:24 -0700

    Updated UT for ChangePassword View Model

--- 5051/5051.UnitTests/Models/ChangePasswordViewModelUnitTests.cs
+++ 5051/5051.UnitTests/Models/ChangePasswordViewModelUnitTests.cs
@@ -13,5 +13,30 @@ namespace _5051.UnitTests.Models
     public class ChangePasswordViewModelUnitTests
     {
         public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Models_ChangePasswordViewModel_Default_Instantiate_Should_Pass()
+        {
+            //arrange
+            var test = new ChangePasswordViewModel();
+
+            var ExpectConfirmPassword = "123";
+            var ExpectNewPassword = "abc";
+            var ExpectOldPassword = "xyz";
+
+
+            //act
+            test.ConfirmPassword = "123";
+            test.NewPassword = "abc";
+            test.OldPassword = "xyz";
+
+            //assert
+            Assert.AreEqual(ExpectConfirmPassword, test.ConfirmPassword, "Confirm " + TestContext.TestName);
+            Assert.AreEqual(ExpectNewPassword, test.NewPassword, "New " + TestContext.TestName);
+            Assert.AreEqual(ExpectOldPassword, test.OldPassword, "Old " + TestContext.TestName);
+        }
+        #endregion Instantiate
+
     }
 }
