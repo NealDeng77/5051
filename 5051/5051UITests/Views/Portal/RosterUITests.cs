2dcf2c239fca6475f967858c80b028fb13ac439f
Troy Osland <oslandt@seattleu.edu>
2018-07-20 16:58:53 -0700

    Added UI tests for portal / roster

---
+++ 5051/5051UITests/Views/Portal/RosterUITests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using static _5051UITests.Extensions;
+
+namespace _5051UITests
+{
+    [TestClass]
+    public class RosterUITests
+    {
+        private string _Controller = "Portal";
+        private string _Action = "Roster";
+
+        [TestMethod]
+        public void Portal_Visit_NavigateToPage_Valid_Should_Pass()
+        {
+            NavigateToPage(AssemblyTests.CurrentDriver, _Controller, _Action);
+        }
+
+    }
+}
