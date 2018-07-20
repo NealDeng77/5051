a684cc17349499f7849036faa75ce4b0dcdc7e3b
Troy Osland <oslandt@seattleu.edu>
2018-07-20 16:35:17 -0700

    Added UI test for portal/visit

---
+++ 5051/5051UITests/Views/Portal/VisitUITTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using static _5051UITests.Extensions;
+
+namespace _5051UITests
+{
+    [TestClass]
+    public class VisitUITTests
+    {
+        private string _Controller = "Portal";
+        private string _Action = "Visit";
+
+        [TestMethod]
+        public void Portal_Visit_NavigateToPage_Valid_Should_Pass()
+        {
+            NavigateToPage(AssemblyTests.CurrentDriver, _Controller, _Action);
+        }
+
+
+    }
+}
