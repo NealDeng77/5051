6c30ad2e98b5e955afe8f552a33ede8c54831cf2
Troy Osland <oslandt@seattleu.edu>
2018-07-23 01:11:33 -0700

    Added functions to test the links in nav bar and footer

--- 5051/5051UITests/Views/Shared/_Layout.cs
+++ 5051/5051UITests/Views/Shared/_Layout.cs
@@ -12,14 +12,58 @@ namespace _5051UITests.Views.Shared
     {
 
 
-        public void Click_All_Nav_Bar_Links(IWebDriver driver, string originalController, string originalAction, string originalData = null)
+        public static void Click_All_Nav_Bar_Links(IWebDriver driver, string originalController, string originalAction, string originalData = null)
         {
+            //find and click a nav bar item
+            //validate the correct page was landed on
+            //return to original page
 
+            driver.FindElement(By.Id("AvatarAttendanceLinkNavBar")).Click();
+            ValidatePageTransition(driver, "Home", "Index");
+            NavigateToPage(driver, originalController, originalAction, originalData);
+
+            driver.FindElement(By.Id("HomeLinkNavBar")).Click();
+            ValidatePageTransition(driver, "Home", "Index");
+            NavigateToPage(driver, originalController, originalAction, originalData);
+
+            driver.FindElement(By.Id("StudentLinkNavBar")).Click();
+            ValidatePageTransition(driver, PortalControllerName, RosterViewName);
+            NavigateToPage(driver, originalController, originalAction, originalData);
+
+            driver.FindElement(By.Id("AdminLinkNavBar")).Click();
+            ValidatePageTransition(driver, "Admin", "Index");
+            NavigateToPage(driver, originalController, originalAction, originalData);
+
+            driver.FindElement(By.Id("KioskModeLinkNavBar")).Click();
+            ValidatePageTransition(driver, "Kiosk", "Login");
+            NavigateToPage(driver, originalController, originalAction, originalData);
         }
 
-        public void Click_All_Footer_Links(IWebDriver driver, string originalController, string originalAction, string originalData = null)
+        public static void Click_All_Footer_Links(IWebDriver driver, string originalController, string originalAction, string originalData = null)
         {
+            //find and click a footer item
+            //validate the correct page was landed on
+            //return to original page
 
+            driver.FindElement(By.Id("AboutLinkFooter")).Click();
+            ValidatePageTransition(driver, "Home", "About");
+            NavigateToPage(driver, originalController, originalAction, originalData);
+
+            driver.FindElement(By.Id("PrivacyPolicyLinkFooter")).Click();
+            ValidatePageTransition(driver, "Home", "Privacy");
+            NavigateToPage(driver, originalController, originalAction, originalData);
+
+            driver.FindElement(By.Id("ContactLinkFooter")).Click();
+            ValidatePageTransition(driver, "Home", "Contact");
+            NavigateToPage(driver, originalController, originalAction, originalData);
+
+            driver.FindElement(By.Id("FAQLinkFooter")).Click();
+            ValidatePageTransition(driver, "Home", "FAQ");
+            NavigateToPage(driver, originalController, originalAction, originalData);
+
+            driver.FindElement(By.Id("UserGuideLinkFooter")).Click();
+            ValidatePageTransition(driver, "Home", "Guide");
+            NavigateToPage(driver, originalController, originalAction, originalData);
         }
 
 
