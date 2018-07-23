db56b36acee6d6dcd34e5cb70448ddfe8fd300bd
Troy Osland <oslandt@seattleu.edu>
2018-07-23 00:33:23 -0700

    added start of functions to test ui of navbar and footer

---
+++ 5051/5051UITests/Views/Shared/_Layout.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using static _5051UITests.Extensions;
+
+namespace _5051UITests.Views.Shared
+{
+
+    public class _Layout
+    {
+
+
+        public void Click_All_Nav_Bar_Links(IWebDriver driver, string originalController, string originalAction, string originalData = null)
+        {
+
+        }
+
+        public void Click_All_Footer_Links(IWebDriver driver, string originalController, string originalAction, string originalData = null)
+        {
+
+        }
+
+
+    }
+}
