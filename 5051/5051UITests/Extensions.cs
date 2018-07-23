cd10a9446c0d38e98fa0ad962d1d04cef280ea1e
Mike Koenig <mikekoenig@live.com>
2018-07-23 16:09:39 -0700

    Updated Game Settings View to have BackButton with ID, and Added UI Automation to call the Game Settings, and test the back button.

--- 5051/5051UITests/Extensions.cs
+++ 5051/5051UITests/Extensions.cs
@@ -18,11 +18,21 @@ namespace _5051UITests
 
         public const int WaitTime = 10;
 
-
         public const string PortalControllerName = "Portal";
         public const string RosterViewName = "Roster";
         public const string ErrorControllerName = "Home";
         public const string ErrorViewName = "Error";
+
+        /// <summary>
+        /// Validates that the controller / action / data page the driver is on 
+        /// is the expected page
+        /// </summary>
+        public static bool ClickActionById(IWebDriver driver, string elementId)
+        {
+            var element = driver.FindElement(By.Id(elementId));
+            element.Click();
+            return true;
+        }
 
         /// <summary>
         /// Validates that the controller / action / data page the driver is on 
---
+++ 5051/5051UITests/Views/GameSettings/UpdateUITests.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using static _5051UITests.Extensions;
+
+namespace _5051UITests.Views.GameSettings
+{
+    [TestClass]
+    public class UpdateUITests
+    {
+        private string _Controller = "GameSettings";
+        private string _Action = "Update";
+
+        [TestMethod]
+        public void Game_Update_NavigateToPage_Valid_Should_Pass()
+        {
+            // Get the data for the record to update
+            NavigateToPage(AssemblyTests.CurrentDriver, _Controller, "Read");
+
+            var element = AssemblyTests.CurrentDriver.FindElement(By.Id("Id"));
+            var elementval = element.GetAttribute("value");
+
+            NavigateToPage(AssemblyTests.CurrentDriver, _Controller, _Action, elementval);
+        }
+
+        [TestMethod]
+        public void Game_Update_Back_Click_Should_Pass()
+        {
+            // Get the data for the record to update
+            NavigateToPage(AssemblyTests.CurrentDriver, _Controller, "Read");
+
+            var element = AssemblyTests.CurrentDriver.FindElement(By.Id("Id"));
+            var elementval = element.GetAttribute("value");
+
+            NavigateToPage(AssemblyTests.CurrentDriver, _Controller, _Action, elementval);
+
+            // Click the Back Button
+            ClickActionById(AssemblyTests.CurrentDriver, "BackButton");
+
+            // Vaidate back on Read page
+            ValidatePageTransition(AssemblyTests.CurrentDriver, _Controller, "Read");
+        }
+
+        [TestMethod]
+        public void Game_Update_NavigateToPage_Invalid_No_ID_Should_See_Error_Page()
+        {
+            AssemblyTests.CurrentDriver.Navigate().GoToUrl(BaseUrl + '/' + _Controller + '/' + _Action);
+
+            ValidatePageTransition(AssemblyTests.CurrentDriver, ErrorControllerName, ErrorViewName);
+        }
+    }
+}
