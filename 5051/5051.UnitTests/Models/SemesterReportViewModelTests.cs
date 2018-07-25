ee87477c0d833c5d8cd42398df84e2cbcbc93e96
Tianhua Zhao <zhaot2@seattleu.edu>
2018-07-25 15:44:32 +0800

    Added unit test for SemesterReportViewModel

--- 5051/5051.UnitTests/Models/SemesterReportViewModelTests.cs
+++ 5051/5051.UnitTests/Models/SemesterReportViewModelTests.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
 
 namespace _5051.UnitTests.Models
 {
     class SemesterReportViewModelTests
     {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void Models_SemesterReportViewModel_Default_Instantiate_Should_Pass()
+        {
+
+            // Act
+            var result = new SemesterReportViewModel();
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
     }
 }
