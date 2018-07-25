f9f4109fa82d636233661098c528e6de76874916
Mike Koenig <mikekoenig@live.com>
2018-07-25 08:30:42 -0700

    Adding jquery typescript definition, and updating the Game typescript.
    Index.cshtml, is the test location for the changes.

--- 5051/5051/Scripts/Game.js
+++ 5051/5051/Scripts/Game.js
@@ -1,17 +1,42 @@
-var Greeter = /** @class */ (function () {
-    function Greeter(message) {
+/// <reference path ="../scripts/jquery/index.d.ts"/>
+function sayHello() {
+    var com = document.getElementById("compiler");
+    var att = com.attributes;
+    var val = att.length;
+    const compiler = "abc"; //(document.getElementById("compiler").attributes;
+    const framework = "xyz"; // (document.getElementById("framework") as HTMLInputElement).value;
+    return `Hello from ${compiler} and ${framework}!`;
+}
+function DataLoad(data) {
+    alert("Data load: " +
+        " Error: " + data.Error +
+        " Msg: " + data.Msg +
+        " Data: " + data.data);
+}
+class Greeter {
+    constructor(message) {
         this.greeting = message;
     }
-    Greeter.prototype.greet = function () {
+    greet() {
         return "Hello, " + this.greeting;
-    };
-    return Greeter;
-}());
-var greeter = new Greeter("world");
-var button = document.createElement('button');
+    }
+}
+let greeter = new Greeter("world");
+let button = document.createElement('button');
 button.textContent = "Say Hello";
 button.onclick = function () {
     alert(greeter.greet());
 };
 document.body.appendChild(button);
+//alert("123");
+//let Mike = sayHello();
+$.ajax("/Game/Simulation", {
+    cache: false,
+    dataType: 'json',
+    type: 'POST'
+})
+    .done(function (data) {
+    console.log(data);
+    DataLoad(data);
+});
 //# sourceMappingURL=game.js.map
--- 5051/5051/Scripts/Game.ts
+++ 5051/5051/Scripts/Game.ts
@@ -1,4 +1,32 @@
-﻿class Greeter {
+﻿/// <reference path ="../scripts/jquery/index.d.ts"/>
+
+function sayHello() {
+
+    var com = document.getElementById("compiler");
+    var att = com.attributes;
+    var val = att.length; 
+
+    const compiler = "abc"; //(document.getElementById("compiler").attributes;
+    const framework = "xyz"; // (document.getElementById("framework") as HTMLInputElement).value;
+    return `Hello from ${compiler} and ${framework}!`;
+}
+
+//let myUrl = URL.parse("http://www.typescriptlang.org");
+
+interface IJsonDataSimulator {
+    Error: string[];
+    Msg: string[];
+    data: number;
+}
+
+function DataLoad(data: IJsonDataSimulator) {
+    alert("Data load: " +
+        " Error: " + data.Error +
+        " Msg: " + data.Msg +
+        " Data: " +data.data);
+}
+
+class Greeter {
     greeting: string;
     constructor(message: string) {
         this.greeting = message;
@@ -17,3 +45,17 @@ button.onclick = function() {
 }
 
 document.body.appendChild(button);
+
+//alert("123");
+//let Mike = sayHello();
+
+$.ajax("/Game/Simulation",
+    {
+        cache: false,
+        dataType: 'json',
+        type: 'POST'
+    })
+    .done(function (data: any) {
+        console.log(data);
+        DataLoad(<IJsonDataSimulator>data);
+    });
