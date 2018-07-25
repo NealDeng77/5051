364a494b71b1c566317a3c1be3cdc7a50d581a11
Tianhua Zhao <zhaot2@seattleu.edu>
2018-07-25 15:58:58 +0800

    Added view model for quarter report

---
+++ 5051/5051/Models/QuarterReportViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5051.Models
+{
+    /// <summary>
+    /// Quarter report view model, for generating quarter report view
+    /// </summary>
+    public class QuarterReportViewModel
+    {
+        /// <summary>
+        /// Contains a select list for semester selection dropdown
+        /// </summary>
+        public List<SelectListItem> Quarters { get; set; }
+
+        /// <summary>
+        /// The selected semester's id for semester selection dropdown
+        /// </summary>
+        public int SelectedQuarterId { get; set; }
+    }
+}
