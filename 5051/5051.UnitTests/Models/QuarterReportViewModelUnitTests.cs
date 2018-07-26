f6e52a7976a0c7788c2532d2448766ab12402ccd
Tianhua Zhao <zhaot2@seattleu.edu>
2018-07-26 08:23:09 -0700

    Added test coverage for QuarterReportViewModel

--- 5051/5051.UnitTests/Models/QuarterReportViewModelUnitTests.cs
+++ 5051/5051.UnitTests/Models/QuarterReportViewModelUnitTests.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using _5051.Models;
 
 namespace _5051.UnitTests.Models
 {
-    class QuarterReportViewModelUnitTests
+    [TestClass]
+    public class QuarterReportViewModelUnitTests
     {
         public TestContext TestContext { get; set; }
 
@@ -21,6 +23,29 @@ namespace _5051.UnitTests.Models
 
             // Assert
             Assert.IsNotNull(result, TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Models_QuarterReportViewModel_InitializeQuarters_Should_Pass()
+        {
+            // Act
+            var data = new QuarterReportViewModel();
+            data.Quarters = new List<SelectListItem>();
+
+            // Assert
+            Assert.IsNotNull(data.Quarters, TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void Models_QuarterReportViewModel_SelectedQuarterId_is_1_Should_Pass()
+        {
+            // Act
+            var data = new QuarterReportViewModel();
+            data.SelectedQuarterId = 1;
+            var expect = 1;
+
+            // Assert
+            Assert.AreEqual(expect, data.SelectedQuarterId, TestContext.TestName);
         }
     }
 }
