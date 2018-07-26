121bc564e4b98673e0f04c31f279c87105f6e45e
Tianhua Zhao <zhaot2@seattleu.edu>
2018-07-26 00:27:38 -0700

    Added unit tests for SchoolYearReportViewModel

---
+++ 5051/5051.UnitTests/Models/SchoolYearReportViewModelUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+
+namespace _5051.UnitTests.Models
+{
+    [TestClass]
+    class SchoolYearReportViewModelUnitTests
+    {
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void Models_StudentReportViewModel_Default_Instantiate_Should_Pass()
+        {
+
+            // Act
+            var result = new SchoolYearReportViewModel();
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+    }
+}
