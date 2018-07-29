77e5a6fc6edb62622d7ad09f80be3c20c43402ee
Mike Koenig <mikekoenig@live.com>
2018-07-28 19:24:12 -0700

    Partial Checkin:  Added ShopTruckItemViewModel and UT for it.  Still working on the Truck Edit

---
+++ 5051/5051.UnitTests/Models/ShopTruckItemViewModelUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+using _5051.Backend;
+
+namespace _5051.UnitTests.Models
+{
+    [TestClass]
+    public class ShopTruckItemViewModelUnitTests
+    {
+        public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Models_ShopTruckItemViewModel_Default_Instantiate_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ShopTruckItemViewModel(null,null);
+
+            // Reset
+            DataSourceBackend.Instance.Reset();
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+
+        }
+        #endregion Instantiate
+    }
+}
