7b07775145f24c2d7947110ac3b39195effe8c6a
Lena Wang <lena@symphoni.cc>
2018-07-28 17:13:57 -0700

    Add Controller_Shop_Inventory_Data_Invalid_StudentID_Null_Should_Fail() test

--- 5051/5051.Tests/Controllers/ShopControllerTest.cs
+++ 5051/5051.Tests/Controllers/ShopControllerTest.cs
@@ -597,10 +597,10 @@ namespace _5051.Tests.Controllers
             data.StudentId = null;
 
             // Act
-            var result = (RedirectToRouteResult)controller.Factory(data);
+            var result = (RedirectToRouteResult)controller.Inventory(data);
 
             // Assert
-            Assert.AreEqual("Factory", result.RouteValues["action"], TestContext.TestName);
+            Assert.AreEqual("Inventory", result.RouteValues["action"], TestContext.TestName);
         }
 
         [TestMethod]
