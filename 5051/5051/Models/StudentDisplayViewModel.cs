c73d9aaf67b71d210a50cc881a43fffdb774e465
Tianhua Zhao <zhaot2@seattleu.edu>
2018-07-30 11:22:01 -0700

    Revert "Revert "Added last login datetime property and emotion uri string property""
    This reverts commit a51c9e5861fdaa6c495e3924e52f80897502b781.

--- 5051/5051/Models/StudentDisplayViewModel.cs
+++ 5051/5051/Models/StudentDisplayViewModel.cs
@@ -40,6 +40,18 @@ namespace _5051.Models
         public DateTime LastDateTime { get; set; }
 
         /// <summary>
+        /// DateTime of last transaction recorded, used for login and logout
+        /// </summary>
+        [Display(Name = "Last Login", Description = "Last Login")]
+        public DateTime LastLogIn { get; set; }
+
+        /// <summary>
+        /// DateTime of last transaction recorded, used for login and logout
+        /// </summary>
+        [Display(Name = "Emotion Image URI", Description = "Emotion Image URI")]
+        public string EmotionImgUri { get; set; }
+
+        /// <summary>
         /// Default constructor
         /// </summary>
         public StudentDisplayViewModel() { }
@@ -66,6 +78,7 @@ namespace _5051.Models
             Password = data.Password;
             Inventory = data.Inventory;
             Attendance = data.Attendance;
+            EmotionCurrent = data.EmotionCurrent;
 
             var myDataAvatar = AvatarBackend.Instance.Read(AvatarId);
             if (myDataAvatar == null)
