3f8029f3204a1904df3ad7d73bde05ee4dc98658
Lena Wang <lena@symphoni.cc>
2018-08-02 21:43:39 -0700

    Created Backend_GameBackend_CustomerPassBy_Qualified_Data_Should_Display_Message() test

--- 5051/5051.UnitTests/Backend/GameBackendUnitTests.cs
+++ 5051/5051.UnitTests/Backend/GameBackendUnitTests.cs
@@ -490,29 +490,29 @@ namespace _5051.UnitTests.Models
 
         #region CustomerPassBy
         [TestMethod]
-        //public void Backend_GameBackend_CustomerPassBy_Qualified_Data_Should_Display_Message()
-        //{
-        //    //arrange
-        //    var test = Backend.GameBackend.Instance;
-        //    var data = test.GetDefault();
-        //    var studentData = new StudentModel();
-        //    var student = Backend.StudentBackend.Instance.Create(studentData);
+        public void Backend_GameBackend_CustomerPassBy_Qualified_Data_Should_Display_Message()
+        {
+            //arrange
+            var test = Backend.GameBackend.Instance;
+            var data = test.GetDefault();
+            var studentData = new StudentModel();
+            var student = Backend.StudentBackend.Instance.Create(studentData);
 
-        //    //act
-        //    student.AvatarLevel = 3;
-        //    student.ExperiencePoints = 20;
+            //act
+            student.AvatarLevel = 3;
+            student.ExperiencePoints = 20;
 
-        //    test.CustomerPassBy(student);
+            test.CustomerPassBy(student);
 
-        //    // Reset
-        //    DataSourceBackend.Instance.Reset();
+            // Reset
+            DataSourceBackend.Instance.Reset();
 
-        //    // Assert
-        //    Assert.IsNotNull(student.Truck.TransactionList, TestContext.TestName);
-        //}
+            // Assert
+            Assert.IsNotNull(student.Truck.TransactionList, TestContext.TestName);
+        }
 
-    #endregion CustomerPassBy
-}
+        #endregion CustomerPassBy
+    }
 
          
 }
