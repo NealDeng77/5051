8bd8a1d19ac7cd8f23ad1b562339c05c4865e391
Mike Koenig <mikekoenig@live.com>
2018-08-07 11:32:32 -0700

    Added AvatarCompositModel and UT

---
+++ 5051/5051/Models/AvatarCompositModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace _5051.Models
+{
+    public class AvatarCompositModel
+    {
+        public string AvatarHeadUri = "/content/avatar/Head0.png";
+        public string AvatarShirtUri = "/content/avatar/Shirt0.png";
+        public string AvatarAccessoryUri = "/content/avatar/Accessory0.png";
+        public string AvatarCheeksUri = "/content/avatar/Cheeks0.png";
+        public string AvatarExpressionUri = "/content/avatar/Expression0.png";
+        public string AvatarHairFrontUri = "/content/avatar/Hair1_straight_black.png";
+        public string AvatarHairBackUri = "/content/avatar/Hair2_short_black.png";
+    }
+}
