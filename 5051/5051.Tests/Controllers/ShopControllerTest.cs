71281f765e21ece68b6f90661c93d272a814957d
Lena Wang <lena@symphoni.cc>
2018-08-13 22:52:32 -0700

    created Controller_Shop_Edit_Data_Valid_Position_Wheels_Should_Pass() test

--- 5051/5051.Tests/Controllers/ShopControllerTest.cs
+++ 5051/5051.Tests/Controllers/ShopControllerTest.cs
@@ -797,6 +797,32 @@ namespace _5051.Tests.Controllers
             Assert.AreEqual(expect, myStudent2.Truck.Sign, TestContext.TestName);
         }
 
+        [TestMethod]
+        public void Controller_Shop_Edit_Data_Valid_Position_Wheels_Should_Pass()
+        {
+            // Arrange
+            ShopController controller = new ShopController();
+            var data = new ShopTruckInputModel();
+            data.StudentId = DataSourceBackend.Instance.StudentBackend.GetDefault().Id;
+            data.ItemId = DataSourceBackend.Instance.FactoryInventoryBackend.GetDefaultTruckFullItem(FactoryInventoryCategoryEnum.Wheels).Id;
+            data.Position = FactoryInventoryCategoryEnum.Wheels;
+
+            // Get the Student Record
+            var myStudent = DataSourceBackend.Instance.StudentBackend.Read(data.StudentId);
+            var expect = myStudent.Truck.Wheels;
+
+            // select item
+            var mySelect = (RedirectToRouteResult)controller.Edit(data);
+
+            // Act
+            ViewResult result = controller.Edit(data) as ViewResult;
+            var myStudent2 = DataSourceBackend.Instance.StudentBackend.Read(data.StudentId);
+            DataSourceBackend.Instance.Reset();
+
+            // Assert
+            Assert.AreEqual(expect, myStudent2.Truck.Wheels, TestContext.TestName);
+        }
+
         #endregion Edit
 
 
