f93bf3061030889433b6889d41bcad2caed3a618
Tianhua Zhao <zhaot2@seattleu.edu>
2018-08-17 01:24:51 -0700

    Updated Update method to include drop down lists.

--- 5051/5051/Models/SchoolDismissalSettingsModel.cs
+++ 5051/5051/Models/SchoolDismissalSettingsModel.cs
@@ -251,6 +251,10 @@ namespace _5051.Models
             EndNormal = data.EndNormal;
             EndEarly = data.EndEarly;
             EndLate = data.EndLate;
+
+            DayStartDropDown = data.DayStartDropDown;
+            DayEndDropDown = data.DayEndDropDown;
+
             DayFirst = data.DayFirst;
             DayLast = data.DayLast;
             FallFirstClassDay = data.FallFirstClassDay;
