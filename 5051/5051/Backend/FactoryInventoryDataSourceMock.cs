2881aa36e5981cdd4205268acb8e9f822d152693
Mike Koenig <mikekoenig@live.com>
2018-08-17 22:22:28 -0700

    Added back in Menu0, Topper0 and Sign0 to default inventory for truck.

--- 5051/5051/Backend/FactoryInventoryDataSourceMock.cs
+++ 5051/5051/Backend/FactoryInventoryDataSourceMock.cs
@@ -177,9 +177,9 @@ namespace _5051.Backend
             //Create(new FactoryInventoryModel("Wheels0.png", "No Wheel", "No Wheels", FactoryInventoryCategoryEnum.Wheels, 1, 1, false));
             //Create(new FactoryInventoryModel("Trailer0.png", "No Trailer", "No Trailer", FactoryInventoryCategoryEnum.Trailer, 1, 1, false));
             //Create(new FactoryInventoryModel("Truck0.png", "No Truck", "No Truck", FactoryInventoryCategoryEnum.Truck, 1, 1, false));
-            //Create(new FactoryInventoryModel("Menu0.png", "No Menu", "No Menu", FactoryInventoryCategoryEnum.Menu, 1, 1, false));
-            //Create(new FactoryInventoryModel("Topper0.png", "No Topper", "No Topper", FactoryInventoryCategoryEnum.Topper, 1, 1, false));
-            //Create(new FactoryInventoryModel("Sign0.png", "No Sign", "No Sign", FactoryInventoryCategoryEnum.Sign, 1, 1, false));
+            Create(new FactoryInventoryModel("Menu0.png", "No Menu", "No Menu", FactoryInventoryCategoryEnum.Menu, 1, 1, false));
+            Create(new FactoryInventoryModel("Topper0.png", "No Topper", "No Topper", FactoryInventoryCategoryEnum.Topper, 1, 1, false));
+            Create(new FactoryInventoryModel("Sign0.png", "No Sign", "No Sign", FactoryInventoryCategoryEnum.Sign, 1, 1, false));
 
             Create(new FactoryInventoryModel("Wheels1.png", "Slick Wheel", "Slick Wheels", FactoryInventoryCategoryEnum.Wheels, 10, 10, false));
             Create(new FactoryInventoryModel("Trailer1.png", "Slick Trailer", "Slick Trailer", FactoryInventoryCategoryEnum.Trailer, 10, 10, false));
