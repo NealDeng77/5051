9f465c9b06023b8bddde89d52b7c3ce97d4b914a
Lena Wang <lena@symphoni.cc>
2018-08-18 21:47:25 -0700

    created businessReportViewModel

---
+++ 5051/5051/Models/BusinessReoprtViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace _5051.Models
+{
+    /// <summary>
+    /// View Model for the GameResult Views to have the list of GameResults
+    /// </summary>
+    public class BusinessReportViewModel
+    {
+        /// <summary>
+        /// The Report Parts
+        /// </summary>
+        public int income;
+        public int outcome;
+        public int profit;
+        public List<TransactionModel> BusinessList;
+
+    }
+}
