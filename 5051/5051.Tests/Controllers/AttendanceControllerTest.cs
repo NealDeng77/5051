4e060c712e9d7cbb930501a44421459c2bab1b04
Tianhua Zhao <zhaot2@seattleu.edu>
2018-08-20 07:51:47 -0700

    Added Controller_Attendance_Instantiate_Default_Should_Pass()

--- 5051/5051.Tests/Controllers/AttendanceControllerTest.cs
+++ 5051/5051.Tests/Controllers/AttendanceControllerTest.cs
@@ -2,11 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using _5051;
+using _5051.Controllers;
+using _5051.Backend;
+using _5051.Models;
 
 namespace _5051.Tests.Controllers
 {
-    class AttendanceControllerTest
+    [TestClass]
+    public class AttendanceControllerTest
     {
+        public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Controller_Attendance_Instantiate_Default_Should_Pass()
+        {
+            // Arrange
+            var controller = new AttendanceController();
+
+            // Act
+            var result = controller.GetType();
+
+            // Assert
+            Assert.AreEqual(result, new AttendanceController().GetType(), TestContext.TestName);
+        }
+
+        #endregion Instantiate
+
+
     }
 }
