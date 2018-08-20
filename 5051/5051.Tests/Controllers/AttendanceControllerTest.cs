58b57229712603a5c4c9e92795aaa4d0a1c6e9a6
Tianhua Zhao <zhaot2@seattleu.edu>
2018-08-20 08:00:25 -0700

    Added Controller_Attendance_Update_Get_Default_Should_Pass()

--- 5051/5051.Tests/Controllers/AttendanceControllerTest.cs
+++ 5051/5051.Tests/Controllers/AttendanceControllerTest.cs
@@ -285,5 +285,26 @@ namespace _5051.Tests.Controllers
             Assert.AreEqual("Error", result.RouteValues["action"], TestContext.TestName);
             Assert.AreEqual("Home", result.RouteValues["controller"], TestContext.TestName);
         }
+
+        [TestMethod]
+        public void Controller_Attendance_Update_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var controller = new AttendanceController();
+
+            var myStudent = StudentBackend.Instance.GetDefault();
+            var myAttendance = new AttendanceModel();
+            var myId = myAttendance.Id;
+            myStudent.Attendance.Add(myAttendance);
+
+            // Act
+            var result = controller.Update(myId) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+
+        #endregion UpdateRegion
+
     }
 }
