eb593ec79f76d221fb0f379ad9724fcb57617274
Mike Koenig <mikekoenig@live.com>
2018-08-20 14:26:22 -0700

    Added AvatarSelect Controller and UT

---
+++ 5051/5051/Controllers/AvatarSelectController.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _5051.Models;
+using _5051.Backend;
+
+namespace _5051.Controllers
+{
+    public class AvatarSelectController : BaseController
+    {
+        private AvatarItemViewModel AvatarItemViewModel = new AvatarItemViewModel();
+
+        // The Backend Data source
+        private AvatarItemBackend AvatarItemBackend = AvatarItemBackend.Instance;
+
+        /// <summary>
+        /// Index to the AvatarSelect
+        /// </summary>
+        /// <returns></returns>
+        // GET: AvatarSelect
+        public ActionResult Index(string id = null)
+        {
+            // Get the Student
+            var myStudent = StudentBackend.Instance.Read(id);
+            if (myStudent == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            // Temp hold the Student Id for the Nav, until the Nav can call for Identity.
+            ViewBag.StudentId = myStudent.Id;
+
+            return View(myStudent);
+        }
+
+        /// <summary>
+        /// What to Buy at the store
+        /// </summary>
+        /// <returns></returns>
+        // GET: Buy
+        public ActionResult Select(string id = null)
+        {
+            // Load the list of data into the AvatarItemList
+            var myData = new SelectedAvatarItemListForStudentViewModel();
+
+            // Get the Student
+            var myStudent = StudentBackend.Instance.Read(id);
+            if (myStudent == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            myData.Student = myStudent;
+
+            // Get the Inventory
+            var InventoryList = AvatarItemBackend.Index();
+
+            // Sort the Inventory into List per Category
+            // Load the ones
+            foreach (var item in Enum.GetValues(typeof(AvatarItemCategoryEnum)))
+            {
+                var temp = new AvatarItemViewModel();
+                temp.Category = (AvatarItemCategoryEnum)item;
+                temp.AvatarItemList = InventoryList.Where(m => m.Category == (AvatarItemCategoryEnum)item).ToList();
+
+                if (temp.AvatarItemList.Any())
+                {
+                    // todo, tag the ones that are already owned
+                    myData.AvatarItemCategoryList.Add(temp);
+                }
+            }
+
+            // Temp hold the Student Id for the Nav, until the Nav can call for Identity.
+            ViewBag.StudentId = myStudent.Id;
+
+            return View(myData);
+        }
+        [HttpPost]
+        public ActionResult Select([Bind(Include=
+                                        "StudentId,"+
+                                        "ItemId,"+
+                                        "")] ShopBuyViewModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Send back for edit, with Error Message
+                return RedirectToAction("Select", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            if (data == null)
+            {
+                // Send to Error Page
+                return RedirectToAction("Error", new { route = "Home", action = "Error" });
+            }
+
+            if (string.IsNullOrEmpty(data.StudentId))
+            {
+                // Send back for Edit
+                return RedirectToAction("Select", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            if (string.IsNullOrEmpty(data.ItemId))
+            {
+                // Send back for Edit
+                return RedirectToAction("Select", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            // Get Student
+            var myStudent = DataSourceBackend.Instance.StudentBackend.Read(data.StudentId);
+            if (myStudent == null)
+            {
+                // Send back for Edit
+                return RedirectToAction("Select", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            // Get Item
+            var myItem = DataSourceBackend.Instance.AvatarItemBackend.Read(data.ItemId);
+            if (myItem == null)
+            {
+                // Send back for Edit
+                return RedirectToAction("Select", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            // Check the Student Token amount, If not enough, return error
+            if (myStudent.Tokens < myItem.Tokens)
+            {
+                // Not enough money...
+                // Send back for Edit
+                return RedirectToAction("Select", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            // check the quantities of item
+            if (myItem.Quantities < 1)
+            {
+                // Not enough quantity...
+                // Send back for Edit
+                return RedirectToAction("Select", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            // Check the Item amount, If not enough, return error
+            if (myItem.Tokens < 1)
+            {
+                // Not enough money...
+                // Send back for Edit
+                return RedirectToAction("Select", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            // Check to see if the student already has the item.  If so, don't buy again, only 1 item per student
+            var ItemAlreadyExists = myStudent.AvatarInventory.Exists(m => m.Id == myItem.Id);
+            if (ItemAlreadyExists)
+            {
+                // Already own it.
+                return RedirectToAction("Select", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            // Reduce the Student Tokens by Item Price
+            myStudent.Tokens -= myItem.Tokens;
+
+            // Time to buy !
+            // Check if the item is limited or not
+            if (myItem.IsLimitSupply == true)
+            {
+                // Reduce the quantities of Item
+                myItem.Quantities -= 1;
+            }                           
+
+            // Add Item to Student Inventory
+            // TODO:  Mike, add inventory to Students...
+            myStudent.AvatarInventory.Add(myItem);
+
+            // Update Student
+            DataSourceBackend.Instance.StudentBackend.Update(myStudent);
+
+            // Temp hold the Student Id for the Nav, until the Nav can call for Identity.
+            ViewBag.StudentId = myStudent.Id;
+
+            return RedirectToAction("Select", "AvatarSelect", new { id = data.StudentId });
+        }
+
+        /// <summary>
+        /// Things on sale at the store
+        /// </summary>
+        /// <returns></returns>
+        // GET: Discounts
+        public ActionResult Discounts()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// store items of the AvatarSelect
+        /// </summary>
+        /// <returns></returns>
+        // GET: Inventory
+        public ActionResult Inventory(string id = null)
+        {
+            var myData = new SelectedAvatarItemListForStudentViewModel();
+
+            // Get the Student
+            var myStudent = StudentBackend.Instance.Read(id);
+            if (myStudent == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            myData.Student = myStudent;
+
+            var InventoryList = myStudent.AvatarInventory;
+
+            foreach (var item in Enum.GetValues(typeof(AvatarItemCategoryEnum)))
+            {
+                var temp = new AvatarItemViewModel();
+                temp.Category = (AvatarItemCategoryEnum)item;
+                temp.AvatarItemList = InventoryList.Where(m => m.Category == (AvatarItemCategoryEnum)item).ToList();
+
+                if (temp.AvatarItemList.Any())
+                {
+                    // todo, tag the ones that are already owned
+                    myData.AvatarItemCategoryList.Add(temp);
+                }
+
+            }
+
+            // Temp hold the Student Id for the Nav, until the Nav can call for Identity.
+            ViewBag.StudentId = myStudent.Id;
+
+            return View(myData);
+        }
+
+        [HttpPost]
+        public ActionResult Inventory([Bind(Include=
+                                        "StudentId,"+
+                                        "ItemId,"+
+                                        "")] ShopBuyViewModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Send back for edit, with Error Message
+                return RedirectToAction("Inventory", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            if (data == null)
+            {
+                // Send to Error Page
+                return RedirectToAction("Error", new { route = "Home", action = "Error" });
+            }
+
+            if (string.IsNullOrEmpty(data.StudentId))
+            {
+                // Send back for Edit
+                return RedirectToAction("Inventory", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            if (string.IsNullOrEmpty(data.ItemId))
+            {
+                // Send back for Edit
+                return RedirectToAction("Inventory", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            // Get Student
+            var myStudent = DataSourceBackend.Instance.StudentBackend.Read(data.StudentId);
+            if (myStudent == null)
+            {
+                // Send back for Edit
+                return RedirectToAction("Inventory", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            // Get Item
+            var myItem = DataSourceBackend.Instance.AvatarItemBackend.Read(data.ItemId);
+            if (myItem == null)
+            {
+                // Send back for Edit
+                return RedirectToAction("Inventory", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            // Update Student
+            DataSourceBackend.Instance.StudentBackend.Update(myStudent);
+
+            return RedirectToAction("Inventory", "AvatarSelect", new { id = data.StudentId });
+
+        }
+        /// <summary>
+        /// Edit The AvatarSelect Details
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Edit(string id=null)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            var studentdata = DataSourceBackend.Instance.StudentBackend.Read(id);
+            if (studentdata == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            if (studentdata.AvatarComposite == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            var data = AvatarItemBackend.GetAvatarShopViewModel(studentdata);
+
+            // Temp hold the Student Id for the Nav, until the Nav can call for Identity.
+            ViewBag.StudentId = studentdata.Id;
+
+            //Return Truck Data
+            return View(data);
+        }
+
+        /// <summary>
+        /// Update the Current Inventory Slot to use tha Item passed up.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Edit([Bind(Include=
+                                        "StudentId,"+
+                                        "ItemId,"+
+                                        "Position,"+
+                                        "")] AvatarItemInputModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Send back for edit, with Error Message
+                return RedirectToAction("Index", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            if (data == null)
+            {
+                return RedirectToAction("Error", new { route = "Home", action = "Error" });
+            }
+
+            if (string.IsNullOrEmpty(data.StudentId))
+            {
+                // Send back for Edit
+                return RedirectToAction("Index", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            if (string.IsNullOrEmpty(data.ItemId))
+            {
+                // Send back for Edit
+                return RedirectToAction("Index", "AvatarSelect", new { id = data.StudentId });
+            }
+           
+
+            // Get Student
+            var myStudent = DataSourceBackend.Instance.StudentBackend.Read(data.StudentId);
+            if (myStudent == null)
+            {
+                // Send back for Edit
+                return RedirectToAction("Index", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            // Get Item
+            var myItem = DataSourceBackend.Instance.AvatarItemBackend.Read(data.ItemId);
+            if (myItem == null)
+            {
+                // Send back for Edit
+                return RedirectToAction("Index", "AvatarSelect", new { id = data.StudentId });
+            }
+
+            switch (data.Position)
+            {
+                case AvatarItemCategoryEnum.Accessory:
+                    myStudent.AvatarComposite.AccessoryId = myItem.Id;
+                    break;
+
+                case AvatarItemCategoryEnum.Cheeks:
+                    myStudent.AvatarComposite.CheeksId = myItem.Id;
+                    break;
+
+                case AvatarItemCategoryEnum.Expression:
+                    myStudent.AvatarComposite.ExpressionId= myItem.Id;
+                    break;
+
+                case AvatarItemCategoryEnum.HairBack:
+                    myStudent.AvatarComposite.HairBackId= myItem.Id;
+                    break;
+
+                case AvatarItemCategoryEnum.HairFront:
+                    myStudent.AvatarComposite.HairFrontId= myItem.Id;
+                    break;
+
+                case AvatarItemCategoryEnum.Head:
+                    myStudent.AvatarComposite.HeadId= myItem.Id;
+                    break;
+
+                case AvatarItemCategoryEnum.Pants:
+                    myStudent.AvatarComposite.PantsId = myItem.Id;
+                    break;
+
+                case AvatarItemCategoryEnum.ShirtFull:
+                    myStudent.AvatarComposite.ShirtFullId = myItem.Id;
+                    break;
+
+                case AvatarItemCategoryEnum.ShirtShort:
+                    myStudent.AvatarComposite.ShirtShortId = myItem.Id;
+                    break;
+            }
+
+            // Update Student
+            DataSourceBackend.Instance.StudentBackend.Update(myStudent);
+
+            return RedirectToAction("Edit", "AvatarSelect", new { id = data.StudentId });
+        }
+
+    }
+}
