ba0dcbb4684de45fcdb2852316a59f03acdd5ac1
Mike Koenig <mikekoenig@live.com>
2018-08-20 20:05:11 -0700

    Added Item to route config for a second paramater, needed until Identity is hooked up.

--- 5051/5051/App_Start/RouteConfig.cs
+++ 5051/5051/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace _5051
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{id}/{item}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, item = UrlParameter.Optional }
             );
         }
     }
