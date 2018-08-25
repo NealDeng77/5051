2ec6d83c7473d72c27deae480a7bae674fc0d167
Tianhua Zhao <zhaot2@seattleu.edu>
2018-08-25 06:42:03 -0700

    Added AdminReportIndexViewModelUnitTests

---
+++ 5051/5051.UnitTests/Models/AdminReportIndexViewModelUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _5051.Models;
+using Microsoft.AspNet.Identity;
+using _5051.Backend;
+
+namespace _5051.UnitTests.Models
+{
+    [TestClass]
+    public class AdminReportIndexViewModelUnitTests
+    {       
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void Models_IndexViewModels_Default_Instantiate_Should_Pass()
+        {
+            //arrange
+            var studentList = StudentBackend.Instance.Index();
+            var expectedLeaderBoard = new List<StudentModel>();
+            var test = new AdminReportIndexViewModel(studentList);
+
+            //act
+            test.Leaderboard = expectedLeaderBoard;
+
+            //assert
+            Assert.AreEqual(expectedLeaderBoard, test.Leaderboard, TestContext.TestName);
+        }
+    }
+}
