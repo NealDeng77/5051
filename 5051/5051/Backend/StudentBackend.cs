5c018bb3534b8cb7bf90b67043a110603151b0cb
Kuo, Chuck <kuoc1@seattleu.edu>
2018-08-26 16:44:20 -0700

    Refactored attendance details viewmodel initialization code to model

--- 5051/5051/Backend/StudentBackend.cs
+++ 5051/5051/Backend/StudentBackend.cs
@@ -256,39 +256,7 @@ namespace _5051.Backend
                 return null;
             }
 
-            var attendanceData = ReadAttendance(id);
-
-            if (attendanceData == null)
-            {
-                return null;
-            }
-
-            var studentId = attendanceData.StudentId;
-
-            var student = Read(studentId);
-
-            if (student == null)
-            {
-                return null;
-            }
-
-            var ret = new AttendanceDetailsViewModel
-            {
-                Attendance = new AttendanceModel
-                {
-                    StudentId = attendanceData.StudentId,
-                    Id = attendanceData.Id,
-                    In = UTCConversionsBackend.UtcToKioskTime(attendanceData.In),
-                    Out = UTCConversionsBackend.UtcToKioskTime(attendanceData.Out),
-                    Emotion = attendanceData.Emotion,
-                    EmotionUri = Emotion.GetEmotionURI(attendanceData.Emotion),
-
-                    IsNew = attendanceData.IsNew
-                },
-                Name = student.Name,
-                AvatarId = student.AvatarId,
-                AvatarComposite = student.AvatarComposite
-            };
+            var ret = new AttendanceDetailsViewModel(id);
 
             return ret;
         }
