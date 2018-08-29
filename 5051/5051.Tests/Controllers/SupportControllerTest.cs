eaae05f44918953082d2def1adf4784e589047dd
karen2800 <Karen2800@hotmail.com>
2018-08-29 13:32:23 -0700

    added UTs for SupportController

---
+++ 5051/5051.Tests/Controllers/SupportControllerTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using _5051;
+using _5051.Controllers;
+using _5051.Backend;
+using _5051.Models;
+
+namespace _5051.Tests.Controllers
+{
+    [TestClass]
+    public class SupportControllerTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            DataSourceBackend.SetTestingMode(true);
+        }
+
+        #region Instantiate
+        [TestMethod]
+        public void Controller_Support_Instantiate_Default_Should_Pass()
+        {
+            // Arrange
+            var controller = new SupportController();
+
+            // Act
+            var result = controller.GetType();
+
+            // Assert
+            Assert.AreEqual(result, new SupportController().GetType(), TestContext.TestName);
+        }
+
+        #endregion Instantiate
+
+        #region CreateStudentRegion
+        [TestMethod]
+        public void Controller_Student_Index_Default_Should_Pass()
+        {
+            // Arrange
+            var controller = new SupportController();
+
+            // Act
+            var result = controller.CreateStudent() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+
+        #endregion CreateStudentRegion
+
+
+    }
+}
