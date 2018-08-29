47ae82c32b23610187b735bfc9dfb72186a3da25
Mike Koenig <mikekoenig@live.com>
2018-08-29 11:04:41 -0700

    Updating DataSourceEnum and UT to match

--- 5051/5051.UnitTests/Models/Enums/DataSourceEnumUnitTests.cs
+++ 5051/5051.UnitTests/Models/Enums/DataSourceEnumUnitTests.cs
@@ -16,12 +16,16 @@ namespace _5051.UnitTests.Models
 
             // Make sure there are no additional values
             var enumCount = DataSourceEnum.GetNames(typeof(DataSourceEnum)).Length;
-            Assert.AreEqual(3, enumCount, TestContext.TestName);
+            Assert.AreEqual(6, enumCount, TestContext.TestName);
 
             // Check each value against their expected value.
             Assert.AreEqual(2, (int)DataSourceEnum.SQL, TestContext.TestName);
             Assert.AreEqual(1, (int)DataSourceEnum.Mock, TestContext.TestName);
             Assert.AreEqual(0, (int)DataSourceEnum.Unknown, TestContext.TestName);
+
+            Assert.AreEqual(10, (int)DataSourceEnum.Local, TestContext.TestName);
+            Assert.AreEqual(11, (int)DataSourceEnum.ServerTest, TestContext.TestName);
+            Assert.AreEqual(12, (int)DataSourceEnum.ServerLive, TestContext.TestName);
         }
         #endregion Instantiate
     }
