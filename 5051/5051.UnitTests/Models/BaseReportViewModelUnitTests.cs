1ad336542836347ef196633809d2359dc3aecf60
Tianhua Zhao <zhaot2@seattleu.edu>
2018-08-30 06:16:23 -0700

    Updated Models_BaseReportViewModel_Get_Set_Check_All_Fields_Should_Pass

--- 5051/5051.UnitTests/Models/BaseReportViewModelUnitTests.cs
+++ 5051/5051.UnitTests/Models/BaseReportViewModelUnitTests.cs
@@ -48,12 +48,26 @@ namespace _5051.UnitTests.Models
             test.AttendanceList = new List<AttendanceReportViewModel>();
             test.Stats = new StudentReportStatsModel();
             test.Goal = 100;
+            test.YearArray = "2018, 2018";
+            test.MonthArray = "1, 1";
+            test.DayArray = "1, 1";
+            test.PerfectValues = "100, 200";
+            test.GoalValues = "80, 160";
+            test.ActualValues = "50, 100";
+            test.EmotionLevelValues = "1, 3";
 
             var expectedStudentId = StudentBackend.Instance.GetDefault().Id;
             var expectedStudent = StudentBackend.Instance.GetDefault();
             var expectedDateStart = new DateTime(2000, 12, 30);
             var expectedDateEnd = new DateTime(2018, 8, 7);
             var expectedGoal = 100;
+            var expectedYearArray = "2018, 2018";
+            var expectedMonthArray = "1, 1";
+            var expectedDayArray = "1, 1";
+            var expectedPerfectValues = "100, 200";
+            var expectedGoalValues = "80, 160";
+            var expectedActualValues = "50, 100";
+            var expectedEmotionLevelValues = "1, 3";
             // Assert
 
             //Check each value
@@ -64,6 +78,13 @@ namespace _5051.UnitTests.Models
             Assert.AreEqual(test.Goal, expectedGoal, "Goal " + TestContext.TestName);
             Assert.IsNotNull(test.AttendanceList, "AttendanceList " + TestContext.TestName);
             Assert.IsNotNull(test.Stats, "Stats " + TestContext.TestName);
+            Assert.AreEqual(test.YearArray, expectedYearArray, "StudentId " + TestContext.TestName);
+            Assert.AreEqual(test.MonthArray, expectedMonthArray, "StudentId " + TestContext.TestName);
+            Assert.AreEqual(test.DayArray, expectedDayArray, "StudentId " + TestContext.TestName);
+            Assert.AreEqual(test.PerfectValues, expectedPerfectValues, "StudentId " + TestContext.TestName);
+            Assert.AreEqual(test.GoalValues, expectedGoalValues, "StudentId " + TestContext.TestName);
+            Assert.AreEqual(test.ActualValues, expectedActualValues, "StudentId " + TestContext.TestName);
+            Assert.AreEqual(test.EmotionLevelValues, expectedEmotionLevelValues, "StudentId " + TestContext.TestName);
         }
         #endregion Get_Set_All_Fields
     }
