c3028ceba0d6801e600fba02bc418762d3cb7fc3
Troy Osland <oslandt@seattleu.edu>
2018-08-29 17:25:40 -0700

    IdentityDataSourcetable now creates records inside the IdentityModel table

--- 5051/5051/Backend/IdentityBackend.cs
+++ 5051/5051/Backend/IdentityBackend.cs
@@ -2,10 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using _5051.Models;
+using System.Web.Mvc;
 
 namespace _5051.Backend
 {
     public class IdentityBackend
     {
+        /// <summary>
+        /// Make into a Singleton
+        /// </summary>
+        private static volatile IdentityBackend instance;
+        private static object syncRoot = new Object();
+
+        private IdentityBackend() { }
+
+        public static IdentityBackend Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new IdentityBackend();
+                            SetDataSource(SystemGlobalsModel.Instance.DataSourceValue);
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        // Get the Datasource to use
+        private static IIdentityInterface DataSource;
+
+
+        /// <summary>
+        /// Sets the Datasource to be Mock or SQL
+        /// </summary>
+        /// <param name="dataSourceEnum"></param>
+        public static void SetDataSource(DataSourceEnum dataSourceEnum)
+        {
+            switch (dataSourceEnum)
+            {
+                case DataSourceEnum.SQL:
+                    break;
+
+                case DataSourceEnum.Local:
+                case DataSourceEnum.ServerLive:
+                case DataSourceEnum.ServerTest:
+                    DataSourceBackendTable.Instance.SetDataSourceServerMode(dataSourceEnum);
+                    DataSource = IdentityDataSourceTable.Instance;
+                    break;
+
+                case DataSourceEnum.Mock:
+                default:
+                    // Default is to use the Mock
+                    DataSource = null;
+                    break;
+            }
+
+        }
+
+
+        public ApplicationUser CreateNewSupportUser(string userName, string password, string id)
+        {
+            var result = DataSource.CreateNewSupportUser(userName, password, id);
+            return result;
+        }
+
     }
 }
--- 5051/5051/Backend/IdentityInterface.cs
+++ 5051/5051/Backend/IdentityInterface.cs
@@ -1,11 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using _5051.Models;
 
 namespace _5051.Backend
 {
-    public class IdentityInterface
+    public interface IIdentityInterface
     {
+        ApplicationUser CreateNewSupportUser(string userName, string password, string supportId);
+
+        ApplicationUser CreateNewTeacher(string teacherName, string teacherPassword, string teacherId);
+
+        ApplicationUser CreateNewStudent(StudentModel student);
+
+        bool UpdateStudent(StudentModel student);
+
+        ApplicationUser FindUserByUserName(string userName);
+
+        ApplicationUser FindUserByID(string id);
+
+        StudentModel GetStudentById(string id);
+
+        List<ApplicationUser> ListAllUsers();
+
+        List<ApplicationUser> ListAllStudentUsers();
+
+        List<ApplicationUser> ListAllTeacherUsers();
+
+        List<ApplicationUser> ListAllSupportUsers();
+
+        bool UserHasClaimOfValue(string userID, string claimType, string claimValue);
+
+        ApplicationUser AddClaimToUser(string userID, string claimTypeToAdd, string claimValueToAdd);
+
+        bool RemoveClaimFromUser(string userID, string claimTypeToRemove);
+
+        bool DeleteUser(ApplicationUser user);
+
+        bool DeleteUser(string id);
+
+        bool LogUserIn(ApplicationUser user, string password);
+
+        bool LogUserIn(string userName, string password);
+
+        bool LogUserOut();
+
+        void Reset();
+
+        void LoadDataSet(DataSourceDataSetEnum setEnum);
     }
 }
