89a6509cbecbb4119819ccd952dc974c2de113e9
Mike Koenig <mikekoenig@live.com>
2018-08-31 20:04:45 -0700

    Fixed UT for Avatar Item Category Enum

--- 5051/5051.UnitTests/Models/Enums/AvatarItemCategoryEnumUnitTests .cs
+++ 5051/5051.UnitTests/Models/Enums/AvatarItemCategoryEnumUnitTests .cs
@@ -20,15 +20,15 @@ namespace _5051.UnitTests.Models
 
             // Check each value against their expected value.
             Assert.AreEqual(0, (int)AvatarItemCategoryEnum.Unknown, TestContext.TestName);
-            Assert.AreEqual(1, (int)AvatarItemCategoryEnum.HairBack, TestContext.TestName);
-            Assert.AreEqual(2, (int)AvatarItemCategoryEnum.Head, TestContext.TestName);
+            Assert.AreEqual(50, (int)AvatarItemCategoryEnum.HairBack, TestContext.TestName);
+            Assert.AreEqual(10, (int)AvatarItemCategoryEnum.Head, TestContext.TestName);
             //Assert.AreEqual(3, (int)AvatarItemCategoryEnum.ShirtShort, TestContext.TestName);
-            Assert.AreEqual(4, (int)AvatarItemCategoryEnum.Expression, TestContext.TestName);
-            Assert.AreEqual(5, (int)AvatarItemCategoryEnum.Cheeks, TestContext.TestName);
-            Assert.AreEqual(6, (int)AvatarItemCategoryEnum.HairFront, TestContext.TestName);
-            Assert.AreEqual(7, (int)AvatarItemCategoryEnum.Accessory, TestContext.TestName);
-            Assert.AreEqual(8, (int)AvatarItemCategoryEnum.ShirtFull, TestContext.TestName);
-            Assert.AreEqual(9, (int)AvatarItemCategoryEnum.Pants, TestContext.TestName);
+            Assert.AreEqual(20, (int)AvatarItemCategoryEnum.Expression, TestContext.TestName);
+            Assert.AreEqual(30, (int)AvatarItemCategoryEnum.Cheeks, TestContext.TestName);
+            Assert.AreEqual(40, (int)AvatarItemCategoryEnum.HairFront, TestContext.TestName);
+            Assert.AreEqual(60, (int)AvatarItemCategoryEnum.Accessory, TestContext.TestName);
+            Assert.AreEqual(70, (int)AvatarItemCategoryEnum.ShirtFull, TestContext.TestName);
+            Assert.AreEqual(80, (int)AvatarItemCategoryEnum.Pants, TestContext.TestName);
         }
         #endregion Instantiate
     }
