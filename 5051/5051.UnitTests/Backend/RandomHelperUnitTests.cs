fdb3f1d8cade8d5264fd22cd3e1b7e0896850943
Lena Wang <lena@symphoni.cc>
2018-09-02 01:50:28 -0700

    updated RandomHelper

---
+++ 5051/5051.UnitTests/Backend/RandomHelperUnitTests.cs
@@ -0,0 +1,36 @@
+﻿using _5051.Backend;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5051.UnitTests.Backend
+{
+    class RandomHelperUnitTests
+    {
+        public TestContext TestContext { get; set; }
+
+        #region Instantiate
+        [TestMethod]
+        public void Backend_RandomHelper_Default_Instantiate_Should_Pass()
+        {
+            // Arrange
+            var backend = RandomHelper.Instance;
+
+            //var expect = backend;
+
+            // Act
+            var result = backend;
+
+            //Reset
+            backend.Reset();
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+
+        #endregion Instantiate
+    }
+}
