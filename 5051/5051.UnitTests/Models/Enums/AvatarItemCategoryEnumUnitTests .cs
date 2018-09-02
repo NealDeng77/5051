af81219e4a46ae27cded92bcb7dcb282efc4f676
Mike Koenig <mikekoenig@live.com>
2018-09-01 19:29:51 -0700

    Added UT coverage for EnumHelper

--- 5051/5051.UnitTests/Models/Enums/AvatarItemCategoryEnumUnitTests .cs
+++ 5051/5051.UnitTests/Models/Enums/AvatarItemCategoryEnumUnitTests .cs
@@ -31,5 +31,34 @@ namespace _5051.UnitTests.Models
             Assert.AreEqual(80, (int)AvatarItemCategoryEnum.Pants, TestContext.TestName);
         }
         #endregion Instantiate
+
+        #region DisplayName
+        
+        [TestMethod]
+        public void Models_AvatarItemCategoryEnumUnitTests_DisplayName_Values_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            // Make sure there are no additional values
+            var enumCount = AvatarItemCategoryEnum.GetNames(typeof(AvatarItemCategoryEnum)).Length;
+           
+            // Reset
+        
+
+            // Assert
+
+            // Check each value against their expected value.
+            Assert.AreEqual("Unknown", AvatarItemCategoryEnum.Unknown.GetDisplayName(), TestContext.TestName);
+            Assert.AreEqual("Hair", AvatarItemCategoryEnum.HairBack.GetDisplayName(), TestContext.TestName);
+            Assert.AreEqual("Head", AvatarItemCategoryEnum.Head.GetDisplayName(), TestContext.TestName);
+            Assert.AreEqual("Expression", AvatarItemCategoryEnum.Expression.GetDisplayName(), TestContext.TestName);
+            Assert.AreEqual("Cheeks", AvatarItemCategoryEnum.Cheeks.GetDisplayName(), TestContext.TestName);
+            Assert.AreEqual("Bangs", AvatarItemCategoryEnum.HairFront.GetDisplayName(), TestContext.TestName);
+            Assert.AreEqual("Accessory", AvatarItemCategoryEnum.Accessory.GetDisplayName(), TestContext.TestName);
+            Assert.AreEqual("Shirt", AvatarItemCategoryEnum.ShirtFull.GetDisplayName(), TestContext.TestName);
+            Assert.AreEqual("Pants", AvatarItemCategoryEnum.Pants.GetDisplayName(), TestContext.TestName);
+        }
+        #endregion
     }
 }
