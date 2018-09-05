419526fe94da7d3a936b4f649441aef44512544e
Tianhua Zhao <zhaot2@seattleu.edu>
2018-09-04 20:22:49 -0700

    Created school calendar backend helper class

---
+++ 5051/5051/Backend/SchoolCalendarBackendHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _5051.Backend
+{
+    /// <summary>
+    /// SchoolCalendarBackend helper functions
+    /// </summary>
+    public static class SchoolCalendarBackendHelper
+    {
+    }
+}
