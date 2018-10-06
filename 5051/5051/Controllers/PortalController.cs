8f525e4e829e626d524258de097b92678ab51ea6
Troy Osland <oslandt@seattleu.edu>
2018-10-06 14:25:33 -0700

    returned portal controller to original state, passwords not needed for login again

--- 5051/5051/Controllers/PortalController.cs
+++ 5051/5051/Controllers/PortalController.cs
@@ -89,15 +89,15 @@ namespace _5051.Controllers
                 return RedirectToAction("Roster", "Portal");
             }
 
-            // When not in testing mode try the password
-            if (!DataSourceBackend.GetTestingMode())
-            {
-                if (!IdentityBackend.Instance.LogUserIn(myStudent.Name, data.Password, _5051.Models.UserRoleEnum.StudentUser, HttpContext))
-                {
-                    ModelState.AddModelError("", "Invalid password");
-                    return View(data);
-                }
-            }
+            //// When not in testing mode try the password
+            //if (!DataSourceBackend.GetTestingMode())
+            //{
+            //    if (!IdentityBackend.Instance.LogUserIn(myStudent.Name, data.Password, _5051.Models.UserRoleEnum.StudentUser, HttpContext))
+            //    {
+            //        ModelState.AddModelError("", "Invalid password");
+            //        return View(data);
+            //    }
+            //}
 
             // all is OK, so redirect to the student index page and pass in the student ID for now.
             return RedirectToAction("Index", "Portal", new { id = data.Id });
@@ -121,14 +121,14 @@ namespace _5051.Controllers
         {
             var CurrentId = IdentityBackend.Instance.GetCurrentStudentID(HttpContext);
 
-            //// Todo: Remove when identity is fully hooked up
-            //// Hack, to keep the current system working, while the identity system is slowly hooked up everywhere.  If the user is not logged in, then the passed in user will work, if the user is logged in, then the passed in user is ignored.
-            //if (string.IsNullOrEmpty(CurrentId))
-            //{
-            //    CurrentId = id;
-            //}
+            // Todo: Remove when identity is fully hooked up
+            // Hack, to keep the current system working, while the identity system is slowly hooked up everywhere.  If the user is not logged in, then the passed in user will work, if the user is logged in, then the passed in user is ignored.
+            if (string.IsNullOrEmpty(CurrentId))
+            {
+                CurrentId = id;
+            }
 
-            //ViewBag.StudentId = CurrentId;  //TODO: Remove this when identity is fully hooked up
+            ViewBag.StudentId = CurrentId;  //TODO: Remove this when identity is fully hooked up
 
             if (IdentityBackend.Instance.BlockExecptForRole(CurrentId, UserRoleEnum.StudentUser))
             {
