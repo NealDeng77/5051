d325b3d37d156133dc2a9dfc5d09d7cbcc6792d1
Mike Koenig <mikekoenig@live.com>
2018-10-12 18:08:55 -0700

    Partial Checkin for Reset Calendar

--- 5051/5051/Controllers/MaintenanceController.cs
+++ 5051/5051/Controllers/MaintenanceController.cs
@@ -5,6 +5,7 @@ using _5051.Backend;
 using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
+//using _5051.Maintenance;
 
 namespace _5051.Controllers
 {
@@ -92,6 +93,73 @@ namespace _5051.Controllers
             DataSourceBackend.Instance.StudentBackend.BackupData(BackupData.Source, BackupData.Destination);
 
             DataSourceBackend.Instance.KioskSettingsBackend.BackupData(BackupData.Source, BackupData.Destination);
+
+            return RedirectToAction("Index", "Maintenance");
+        }
+
+
+        public ActionResult ResetCalendar()
+        {
+            // TODO: Troy
+            // Lookup Current user, make sure they are in the support list.
+            //var findResult = IdentityBackend.Instance.FindUserByID(id);
+            //if (findResult == null)
+            //{
+            //return RedirectToAction("UserList", "Support");
+            //}
+
+            var data = new LoginViewModel
+            {
+                Password = "",
+                Email = ""
+            };
+
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult ResetCalendar([Bind(Include =
+                                            "Password," +
+                                            "Email," +
+                                             "")] LoginViewModel data)
+        {
+            // Todo: Troy
+            // Check for Valid User
+            //return RedirectToAction("UserList", "Support");
+
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Maintenance");
+            }
+
+            if (string.IsNullOrEmpty(data.Password))
+            {
+                return RedirectToAction("Index", "Maintenance");
+            }
+
+            // TODO:  Troy, changes this from 123, to be support password via code lookup
+            // User will already be logged in, so this is more just a double check that they want to do the operation
+            if (!data.Password.Equals("123"))
+            {
+                return RedirectToAction("Index", "Maintenance");
+            }
+
+
+            /*
+             * Allen
+             * 
+             * So investigate what is wrong with the current calendar data
+             * I am assuming each calendar record needs to be inspected and potentialy fixed up
+             * 
+             * Best way to do this is to make Unit Tests for each of the issues that can be fixed, write the unit test first,  then the code to fix it.
+             * Repeat untill all issues are fixed
+             * 
+             */
+
+            //var temp = new ;
+
+            //.Maintenance.CalendarMaintenance();
+
 
             return RedirectToAction("Index", "Maintenance");
         }
