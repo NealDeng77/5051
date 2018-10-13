d4773ce5ab15d6b7bd26e83ef21ae4e3dae43411
Mike Koenig <mikekoenig@live.com>
2018-10-12 19:12:03 -0700

    Adding AvatarItem Maintenance and UTs

---
+++ 5051/5051/Maintain/AvatarItemMaintenance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using _5051.Backend;
+using _5051.Models;
+
+namespace _5051.Maintain
+{
+
+    public class AvatarItemMaintenance
+    {
+        /// <summary>
+        /// Refersh Avatars, walks the Avatar Data in the DB, with the Avatar Defaults from the Helper, and restores defaults that are not there. 
+        /// Call this function after uploading new avatars etc
+        /// Or changing an avatar settings
+        /// </summary>
+        public bool RefreshAvatars()
+        {
+
+            return true;
+        }
+    }
+}
