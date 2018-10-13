b5555f97d524568e45840b92c46eb13b3ac64a7a
Mike Koenig <mikekoenig@live.com>
2018-10-12 18:26:42 -0700

    Adding UT for Calender and DataBackup Maintenance

---
+++ 5051/5051.Tests/Maintain/DataBackupMaintenance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using _5051;
+using _5051.Maintain;
+using _5051.Backend;
+using _5051.Models;
+
+namespace _5051.Tests.Maintenance
+{
+    [TestClass]
+    public class DataBackupMaintenance
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            DataSourceBackend.SetTestingMode(true);
+        }
+
+        #region Instantiate
+        [TestMethod]
+        public void Maintenance_DataBackup_Instantiate_Default_Should_Pass()
+        {
+            // Arrange
+            var Maintenance = new DataBackupMaintenance();
+
+            // Act
+            var result = Maintenance.GetType();
+
+            // Assert
+            Assert.AreEqual(result, new DataBackupMaintenance().GetType(), TestContext.TestName);
+        }
+
+        #endregion Instantiate
+
+
+    }
+}
