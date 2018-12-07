2ae555da61ff0d2e9e225df2f0f33602db48a1ca
Mike Koenig <mikekoenig@live.com>
2018-12-07 12:04:28 -0800

    readonly

--- 5051/5051/Backend/DataSourceBackendTable.cs
+++ 5051/5051/Backend/DataSourceBackendTable.cs
@@ -20,7 +20,7 @@ namespace _5051.Backend
         /// Make into a Singleton
         /// </summary>
         private static volatile DataSourceBackendTable instance;
-        private static object syncRoot = new Object();
+        private static readonly object syncRoot = new Object();
 
         private DataSourceBackendTable() { }
 
